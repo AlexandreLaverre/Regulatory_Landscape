9066eb3b52fc09352a7a02ce6af94e88698f8b49
Alexandre Laverré <alexandre.laverre@etu.umontpellier.fr>
2019-02-13 15:31:49 +0100

    correction overlap sur start + generalisation

--- overlap/Overlap.py
+++ overlap/Overlap.py
@@ -11,7 +11,7 @@ interest_file = sys.argv[2]
 output_file = sys.argv[3]
 
 
-def sorting_dictionary(file):
+def sorted_dictionary(file):
     dic = {}
     with open(file, 'r') as f:
         start = 0
@@ -22,10 +22,10 @@ def sorting_dictionary(file):
         for i in f.readlines()[start:]:
             i = i.strip("\n")
             i = i.split("\t")
-            pos = (int(i[1]), int(i[2]), str(i[3]))  # values = positions + ID
+            pos = (int(i[1]), int(i[2]), str(i[3]))  # value = position (start + end) + ID
 
             if i[0] in dic.keys():
-                dic[i[0]].append(pos)     # keys = chromosomes
+                dic[i[0]].append(pos)     # keys = chromosome
             else:
                 dic[i[0]] = [pos]
 
@@ -37,13 +37,14 @@ def sorting_dictionary(file):
     return dic
 
 
-ref_dic = sorting_dictionary(reference_file)
-print("Reference dictionary ready !")
+ref_dic = sorted_dictionary(reference_file)
+print("Reference dictionary ready")
 
-int_dic = sorting_dictionary(interest_file)
-print(int_dic)
+int_dic = sorted_dictionary(interest_file)
+
 # Testing overlap in interest dic
 if list(int_dic.values())[0][0][0] != list(int_dic.values())[0][0][1]:
+    print("Length of interest seq. > 1 pb ")
     new_int_dic = {}
     for k in int_dic.keys():
         current_start = int_dic[k][0][0]
@@ -71,13 +72,17 @@ if list(int_dic.values())[0][0][0] != list(int_dic.values())[0][0][1]:
     new_nb_elem = sum(len(new_int_dic[x]) for x in new_int_dic.keys())
     if nb_elem != new_nb_elem:
         print("There is overlap in interest file !")
+    else:
+        print("There is no overlap in interest file !")
 
     int_dic = new_int_dic
 
-print(int_dic)
-print("Reference dictionary ready !")
-print("Running overlap... ")
+else:
+    print("Length of interest seq. = 1 pb")
 
+
+print("Interest dictionary ready")
+print("Running overlap... ")
 
 # Overlap interest to reference
 dic_output = {}
@@ -91,12 +96,12 @@ for chr in ref_dic.keys():
             ref_pos = str(chr) + "\t" + str(start) + "\t" + str(end)
 
             # Initialization of first possible overlapping interest position
-            while i < len(int_dic[chr]) and int_dic[chr][i][1] <= start:
+            while i < len(int_dic[chr]) and int_dic[chr][i][1] < start:
                 i += 1
             first_i = i
 
             # Adding all overlapping interest position to reference position
-            while i < len(int_dic[chr]) and int_dic[chr][i][0] < end:
+            while i < len(int_dic[chr]) and int_dic[chr][i][0] <= end:
                 if ref_pos in dic_output.keys():
                     dic_output[ref_pos].append(int_dic[chr][i])
                 else:
@@ -109,9 +114,10 @@ for chr in ref_dic.keys():
 
 
 print("Writting output... ")
+
 output = open(output_file, 'w')
 if os.stat(output_file).st_size == 0:
-    output.write("chr\tstart\tend\toverlap_start\toverlap_end\toverlap_ID\n")
+    output.write("chr\tstart\tend\toverlap_ID\n")
 
 for ref_pos, int_pos in dic_output.items():
     output.write(ref_pos + "\t")
@@ -120,9 +126,10 @@ for ref_pos, int_pos in dic_output.items():
     for i in int_pos:
         count += 1
         if count == len(int_pos):
-            output.write(str(i[0]) + "\t" + str(i[1]) + "\t" + str(i[2]))
+            output.write(str(i[2]))
             output.write("\n")
+        else:
+            output.write(str(i[2]) + ",")
 
 output.close()
 print("All done !")
-
