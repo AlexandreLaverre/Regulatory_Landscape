11c27d93cd84585f61e0f54a12038c5ea2ea172f
Alexandre Laverré <alexandre.laverre@etu.umontpellier.fr>
2019-03-22 14:54:34 +0100

    correction collapse

--- overlap/Overlap.py
+++ overlap/Overlap.py
@@ -44,45 +44,58 @@ print("Reference dictionary ready")
 
 int_dic = sorted_dictionary(interest_file)
 
+
 # Testing overlap in interest dic
-if list(int_dic.values())[0][0][0] != list(int_dic.values())[0][0][1]:
-    print("Length of interest seq. > 1 pb ")
-    if collapse == "T":
-        new_int_dic = {}
-        for k in int_dic.keys():
-            current_start = int_dic[k][0][0]
-            current_end = int_dic[k][0][1]
-            current_ID = int_dic[k][0][2]
+def collapse_intraoverlap(dic):
+    if list(dic.values())[0][0][0] != list(dic.values())[0][0][1]:
+        print("Length of", name_dic, "seq. > 1 pb ")
+        if collapse == "T":
+            new_dic = {}
+            for k in dic.keys():
+                current_start = dic[k][0][0]
+                current_end = dic[k][0][1]
+                current_ID = dic[k][0][2]
 
-            new_int_dic[k] = []
-            for i in range(1, len(int_dic[k])):
-                new_start = int_dic[k][i][0]
-                new_end = int_dic[k][i][1]
-                new_ID = int_dic[k][i][2]
-                if current_end >= new_start >= current_start:
-                    current_end = new_end
-                    current_ID = str(current_ID) + "_" + str(new_ID)
-                else:
-                    new_int_dic[k].append((current_start, current_end, current_ID))
-                    current_start = new_start
-                    current_end = new_end
-                    current_ID = new_ID
+                new_dic[k] = []
+                if len(dic[k]) == 1:
+                    new_dic[k].append((current_start, current_end, current_ID))
 
-                if i == len(int_dic[k])-1:
-                    new_int_dic[k].append((current_start, current_end, current_ID))
+                for i in range(1, len(dic[k])):
+                    new_start = dic[k][i][0]
+                    new_end = dic[k][i][1]
+                    new_ID = dic[k][i][2]
+                    if current_end >= new_start >= current_start:
+                        if current_end > new_end:
+                            current_end = new_end
+                        current_ID = str(current_ID) + "_" + str(new_ID)
 
-        nb_elem = sum(len(int_dic[x]) for x in int_dic.keys())
-        new_nb_elem = sum(len(new_int_dic[x]) for x in new_int_dic.keys())
-        if nb_elem != new_nb_elem:
-            print("There is overlap in interest file !")
-        else:
-            print("There is no overlap in interest file !")
+                    else:
+                        new_dic[k].append((current_start, current_end, current_ID))
+                        current_start = new_start
+                        current_end = new_end
+                        current_ID = new_ID
 
-        int_dic = new_int_dic
+                    if i == len(dic[k])-1:
+                        new_dic[k].append((current_start, current_end, current_ID))
 
-else:
-    print("Length of interest seq. = 1 pb")
+            nb_elem = sum(len(dic[x]) for x in dic.keys())
+            new_nb_elem = sum(len(new_dic[x]) for x in new_dic.keys())
+            if nb_elem != new_nb_elem:
+                print("There is overlap in", name_dic, "file !")
+                print("Before collapse:", nb_elem, "elements; After collapse:", new_nb_elem)
+            else:
+                print("There is no overlap in", name_dic, "file !")
 
+            return new_dic
+
+    else:
+        print("Length of", name_dic, "seq. = 1 pb")
+
+
+name_dic = 'reference'
+ref_dic = collapse_intraoverlap(ref_dic)
+name_dic = 'interest'
+int_dic = collapse_intraoverlap(int_dic)
 
 print("Interest dictionary ready")
 print("Running overlap... ")
@@ -94,18 +107,18 @@ for chr in ref_dic.keys():
     for pos in ref_dic[chr]:
         start = pos[0]
         end = pos[1]
-        ref_pos = str(chr) + "\t" + str(start) + "\t" + str(end)
+        ref_pos = str(chr) + "\t" + str(start) + "\t" + str(end) #+ "\t" + str(pos[2])
 
         if chr in int_dic.keys():
 
             # Initialization of first possible overlapping interest position
             i = first_i
-            while i < len(int_dic[chr]) and int_dic[chr][i][1] < start:
+            while i < len(int_dic[chr]) and int_dic[chr][i][1] < start:  # -250:
                 i += 1
             first_i = i
 
             # Adding all overlapping interest position to reference position
-            while i < len(int_dic[chr]) and int_dic[chr][i][0] <= end:
+            while i < len(int_dic[chr]) and int_dic[chr][i][0] <= end:  #+ 250:
                 if ref_pos in dic_output.keys():
                     dic_output[ref_pos].append(int_dic[chr][i])
                 else:
@@ -131,9 +144,9 @@ for ref_pos in dic_output.keys():
             overlap_start = int(overlap[0])
             overlap_end = int(overlap[1])
 
-            if overlap_start < int(frag_pos[1]):
+            if overlap_start < int(frag_pos[1]):  # -250:
                 overlap_start = int(frag_pos[1])
-            if overlap_end > int(frag_pos[2]):
+            if overlap_end > int(frag_pos[2]):  # +250:
                 overlap_end = int(frag_pos[2])
 
             length_overlap = overlap_end - overlap_start
@@ -155,16 +168,24 @@ if os.stat(output_file).st_size == 0:
 
 for ref_pos, int_pos in dic_output.items():
     output.write(ref_pos + "\t")
+    chr = ref_pos.strip('\t')[0]
     count = 0
 
     for i in int_pos:
         count += 1
         if count == len(int_pos):
-            output.write(str(i[2]) + "\t" + str(length_pos[ref_pos]) + "\t" + str(count_bp[ref_pos]) + '\t' +
-                         str((count_bp[ref_pos]/length_pos[ref_pos])*100) + "\n")
+            if str(i[0]) == 'NA':
+                output.write('NA' + "\t" + str(length_pos[ref_pos]) + "\t" +
+                             str(count_bp[ref_pos]) + '\t' + str((count_bp[ref_pos] / length_pos[ref_pos]) * 100) + "\n")
+
+            else:
+                output.write(str(chr)+':'+str(i[0])+':'+str(i[1]) + "\t" + str(length_pos[ref_pos]) + "\t" +
+                             str(count_bp[ref_pos]) + '\t' + str((count_bp[ref_pos]/length_pos[ref_pos])*100) + "\n")
             #output.write(str(i[2])+":"+str(i[0])+"-"+str(i[1])+"\n")
         else:
-            output.write(str(i[2]) + ",")
+
+            output.write(str(chr) + ':' + str(i[0]) + ':' + str(i[1])+',')
+            #output.write(str(i[2]) + ",")
             #output.write(str(i[2])+":"+str(i[0])+"-"+str(i[1]) + ",")
 
 output.close()
