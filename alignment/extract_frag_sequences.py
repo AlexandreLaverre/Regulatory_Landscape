b2537e1a2e3f9940980f6c36f275688f5f29a1df
Alexandre Laverré <alexandre.laverre@etu.umontpellier.fr>
2019-05-20 17:34:55 +0200

    restriction frag of 2 species in same time

--- alignment/extract_frag_sequences.py
+++ alignment/extract_frag_sequences.py
@@ -2,38 +2,46 @@
 # coding=utf-8
 
 from Bio import SeqIO
-import sys
 
 sp = "human" #sys.argv[2]
-sp2 = "human" #sys.argv[1]
+sp2 = "mouse" #sys.argv[1]
 
 path_data = "/home/laverre/Documents/Regulatory_Landscape/data/"
-ref_genome = path_data+sp2+"/genome/"+sp2+".GRCh38.dna_rm.primary_assembly.fa"
+ref_genome = path_data+sp+"/genome/"+sp+".GRCh38.dna_sm.primary_assembly.fa"
 ref_dict = SeqIO.to_dict(SeqIO.parse(ref_genome, "fasta"))
 
-#"+sp+"2"+sp2+"_restriction_fragments_0.4.bed", 'r') as f1:
-with open(path_data+sp2+"/genome/Digest_hg38_HindIII_None.bed", "r") as f1:
-    chr_old = 1
-    output = open(path_data + sp2 + "/genome/human_restriction_fragments_mask.fa_chr1", "w")
+target_genome = path_data+sp2+"/genome/"+sp2+".GRCm38.dna_sm.primary_assembly.fa"
+target_dict = SeqIO.to_dict(SeqIO.parse(target_genome, "fasta"))
+
+
+with open(path_data+sp+"/genome/"+sp+"2"+sp2+"_restriction_fragments_0.1.bed", "r") as f1:
+    output_sp1 = open(path_data + sp + "/genome/"+sp+"_restriction_fragments_softmask.fa", "w")
+    output_sp2 = open(path_data + sp2 + "/genome/"+sp+"2"+sp2+"_restriction_fragments_softmask.fa", "w")
     for i in f1.readlines():
         i = i.split("\t")
-        chr = i[0].strip("chr")
-        if chr != chr_old:
-            output.close()
-            output = open(path_data + sp2 + "/genome/human_restriction_fragments_mask.fa"+"_chr"+chr, "w")
-            chr_old = chr
-        # "+sp+"2"+sp2+"_restriction_fragments_0.4.fa", "w")
+        frag_ref = i[3].split(':')
+        chr_ref = frag_ref[0].strip("chr")
 
-        if chr in ref_dict.keys():
-            sequence = ref_dict[chr]
-            cut_sequence = sequence[int(i[1]):int(i[2])]
+        if chr_ref in ref_dict.keys():
+            sequence_ref = ref_dict[chr_ref]
+            cut_sequence = sequence_ref[int(frag_ref[1]):int(frag_ref[2])]
+            cut_sequence.id = str(i[3] + ":+")
+
+            SeqIO.write(cut_sequence, output_sp1, "fasta")
+
+        chr_target = i[0].strip("chr")
+
+        if chr_target in target_dict.keys():
+            sequence_target = ref_dict[chr_ref]
+            cut_sequence = sequence_target[int(i[1]):int(i[2])]
             if i[5].strip('\n') == "-":
                 cut_sequence = cut_sequence.reverse_complement()
 
             cut_sequence.id = str(i[3] + ":+")
-            print(cut_sequence)
-            #cut_sequence.description = "position in ref_genome"
+            cut_sequence.description = "position in ref_genome"
 
-            SeqIO.write(cut_sequence, output, "fasta")
+            SeqIO.write(cut_sequence, output_sp2, "fasta")
 
-output.close()
+output_sp1.close()
+output_sp2.close()
+
