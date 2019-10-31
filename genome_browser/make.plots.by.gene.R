1691cdd6dd5c7284765bf32bf168fab9f5e3eb77
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2019-10-31 14:50:47 +0100

    added plot annotations

---
+++ genome_browser/make.plots.by.gene.R
@@ -0,0 +1,98 @@
+######################################################################
+
+objects=ls()
+
+if(!("load"%in%objects)){
+  load=TRUE
+}
+
+######################################################################
+
+if(load==TRUE){
+  load("RData/data.annotations.RData")
+  load("RData/data.interactions.per.sample.RData")
+  load("RData/data.interactions.annotations.RData")
+
+  load=FALSE
+}
+
+source("plot.annotations.R")
+
+######################################################################
+
+## sp="human"
+## samples= names(interactions[[sp]])
+## nbsamples=length(samples)
+## gene="ENSG00000164690"
+  
+## gene.annot=gene.coords[[sp]]
+## this.gene.start=gene.annot$start[which(gene.annot$id==gene)]
+## this.gene.end=gene.annot$end[which(gene.annot$id==gene)]
+
+## bait.annot=annot.baits.TSS[[sp]]
+## bait.thisgene=bait.annot[which(bait.annot$gene_ID==gene),]
+
+## interactions.thisgene=data.frame()
+
+## for(sample in samples){
+##   this.int=interactions[[sp]][[sample]]
+##   this.int$bait_ID=paste(this.int$bait_chr, this.int$bait_start, this.int$bait_end, sep=",")
+##   this.int=this.int[which(this.int$bait_ID%in%bait.thisgene$bait_ID),]
+
+##   if(dim(this.int)[1]>0){
+##     this.int$sample=rep(sample, dim(this.int)[1])
+
+##     if(dim(interactions.thisgene)[1]>0){
+##       interactions.thisgene=rbind(interactions.thisgene, this.int, stringsAsFactors=F)
+##     } else{
+##       interactions.thisgene=this.int
+##     }
+##   }
+## }
+
+## xstart=min(c(this.gene.start, interactions.thisgene$otherEnd_start))
+## xend=max(c(this.gene.end, interactions.thisgene$otherEnd_end))
+
+######################################################################
+
+figheight=(nbsamples+1)*0.25
+
+pdf(file=paste("figures/", gene, "_",xstart,"_", xend, ".pdf",sep=""), width=8, height=figheight)
+
+######################################################################
+
+m=matrix(rep(NA, nbsamples+2), nrow=nbsamples+2)
+
+for(i in 1:2){
+  m[i,]=1
+}
+
+for(i in 3:(nbsamples+2)){
+  m[i,]=i-1
+}
+
+layout(m)
+
+######################################################################
+
+par(mar=c(1.5, 2.1, 0.25, 1.1))
+plot.annotations(gene.annot, focus.gene=gene, xlim=c(xstart, xend))
+
+######################################################################
+
+for(sample in samples){
+  plot(1, type="n", xlim=c(xstart, xend), ylim=c(0, 1), xaxs="i", yaxs="i", xlab="", axes=F)
+  
+  this.int=interactions.thisgene[which(interactions.thisgene$sample==sample),]
+
+  if(dim(this.int)[1]>0){
+    rect(this.int$otherEnd_start, 0.15, this.int$otherEnd_end, 0.95, col="red", border="red")
+  }
+  
+}
+
+######################################################################
+
+dev.off()
+
+######################################################################
---
+++ genome_browser/plot.annotations.R
@@ -0,0 +1,65 @@
+#####################################################################
+
+plot.annotations<-function(gene.coords, focus.gene, xlim, col.focus="navy", col.other="gray60", axis=T){
+
+  if(!focus.gene%in%gene.coords$id){
+    stop(paste("cannot find", focus.gene, "in annotations"))
+  }
+  ## extract chr
+  this.chr=gene.coords$chr[which(gene.coords$id==focus.gene)]
+  this.genes=gene.coords[which(gene.coords$chr==this.chr & ((gene.coords$start>=xlim[1] & gene.coords$start<=xlim[2]) | (gene.coords$end>=xlim[1] & gene.coords$end<=xlim[2]) | (gene.coords$start<=xlim[1] & gene.coords$end>=xlim[2]))),]
+
+  if(!focus.gene%in%this.genes$id){
+   stop(paste(focus.gene, "does not overlap with region")) 
+  }
+
+  this.genes$color=col.other
+  this.genes$color[which(this.genes$id==focus.gene)]=col.focus
+  
+  print(paste(dim(this.genes)[1], "genes"))
+  
+  ylim=c(-0.3,0.5)
+  ypos=0
+  height=0.2
+
+  arrowheight=0.25
+  arrowsize=diff(xlim)/30
+  
+  plot(1, type="n", xlab="", ylab="", xlim=xlim, ylim=ylim, axes=F, xaxs="i", yaxs="i") ## empty plot
+  
+  ## abline(h=0, col="gray40")
+
+  for(g in this.genes$id){
+    this.strand=this.genes$strand[which(this.genes$id==g)]
+    this.col=this.genes$color[which(this.genes$id==g)]
+    this.start=this.genes$start[which(this.genes$id==g)]
+    this.end=this.genes$end[which(this.genes$id==g)]
+    this.name=this.genes$name[which(this.genes$id==g)]
+
+    if(this.start<xlim[1]){
+      this.start=xlim[1]
+    }
+
+    if(this.end>xlim[2]){
+      this.end=xlim[2]
+    }
+ 
+    rect(this.start, ypos-height/2, this.end, ypos+height/2, col=this.col, border=this.col)
+
+    if(this.strand=="+"){
+      segments(this.start, ypos+height/2, this.start, ypos+height/2+arrowheight)
+      arrows(this.start, ypos+height/2+arrowheight, this.start+arrowsize, ypos+height/2+arrowheight, col="gray20", length=0.075, xpd=NA, lwd=1.5)
+    } else{
+      segments(this.end, ypos+height/2, this.end, ypos+height/2+arrowheight)
+      arrows(this.end, ypos+height/2+arrowheight, this.end-arrowsize, ypos+height/2+arrowheight, col="gray20", length=0.075, xpd=NA, lwd=1.5)
+    }
+
+    text(this.name, x=(this.start+this.end)/2, y=ypos+height*0.75, adj=c(0.5, 0), cex=1.1, font=3)
+  }
+
+  if(axis){
+    axis(side=1, mgp=c(3, 0.5, 0))
+  }
+}
+
+#####################################################################
