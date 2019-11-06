23071ae6a2ccb279d1601a3d530e42e0dfd5bcfe
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2019-11-06 15:55:48 +0100

    corrected bug in case of missing enhancer files

--- genome_browser/data.enhancers.R
+++ genome_browser/data.enhancers.R
@@ -15,18 +15,21 @@ for(sp in c("human", "mouse")){
   enhancer.coords[[sp]]=list()
 
   files=system(paste("ls ", pathEnhancers, sp, "/coord_enh/ | grep .bed",sep=""), intern=T)
-  datasets=unlist(lapply(files, function(x) unlist(strsplit(x, split="_"))[1]))
 
-  for(i in 1:length(files)){
-    dataset=datasets[i]
-
-    print(dataset)
-
-    this.coords=read.table(paste(pathEnhancers, sp, "/coord_enh/", files[i], sep=""), h=T, stringsAsFactors=F, sep="\t")
-    this.coords$chr=unlist(lapply(this.coords$chr, function(x) substr(x, 4, nchar(x))))
-    this.coords=this.coords[, c("chr", "start", "end")]
-
-    enhancer.coords[[sp]][[dataset]]=this.coords
+  if(length(files)>0){
+    datasets=unlist(lapply(files, function(x) unlist(strsplit(x, split="_"))[1]))
+    
+    for(i in 1:length(files)){
+      dataset=datasets[i]
+      
+      print(dataset)
+      
+      this.coords=read.table(paste(pathEnhancers, sp, "/coord_enh/", files[i], sep=""), h=T, stringsAsFactors=F, sep="\t")
+      this.coords$chr=unlist(lapply(this.coords$chr, function(x) substr(x, 4, nchar(x))))
+      this.coords=this.coords[, c("chr", "start", "end")]
+      
+      enhancer.coords[[sp]][[dataset]]=this.coords
+    }
   }
 }
 
