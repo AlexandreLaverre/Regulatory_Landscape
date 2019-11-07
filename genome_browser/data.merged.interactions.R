0a57a76638bba8a5fe6050080557c0e6b3efb1d7
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2019-11-07 10:53:16 +0100

    maximum distance 10Mb for merged interactions

--- genome_browser/data.merged.interactions.R
+++ genome_browser/data.merged.interactions.R
@@ -7,6 +7,10 @@ pathInteractions=paste(pathAlex, "result/Supplementary_dataset2_merged_fragments
 
 #####################################################################
 
+maxdist=10e6 ## 10Mb maximum distance between bait and contacted position
+
+#####################################################################
+
 merged.interactions=list()
 
 for(sp in c("human", "mouse")){
@@ -14,6 +18,12 @@ for(sp in c("human", "mouse")){
   this.mi=read.table(paste(pathInteractions, sp,"/all_interactions_merged.txt", sep=""), h=T, stringsAsFactors=F, sep="\t")
   this.mi$bait_chr=unlist(lapply(this.mi$bait_chr, function(x) substr(x, 4, nchar(x))))
   this.mi$chr=unlist(lapply(this.mi$chr, function(x) substr(x, 4, nchar(x))))
+
+  this.mi$bait_midpos=(this.mi$bait_start+this.mi$bait_end)/2
+  this.mi$otherEnd_midpos=(this.mi$start+this.mi$end)/2
+  this.mi$distance=abs(this.mi$bait_midpos-this.mi$otherEnd_midpos)
+
+  this.mi=this.mi[which(this.mi$distance<=maxdist),]
   
   this.mi$bait_ID=paste(this.mi$bait_chr, this.mi$bait_start, this.mi$bait_end, sep=",")
 
