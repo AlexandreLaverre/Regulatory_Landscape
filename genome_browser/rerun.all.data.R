e24b2abe2a3fed187bd7f43ad8b8ccb729ed22a2
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2019-11-19 16:27:52 +0100

    added script to rerun all data

---
+++ genome_browser/rerun.all.data.R
@@ -0,0 +1,11 @@
+#####################################################################
+
+for(file in c("data.gene.annotations.R", "data.bait.annotations.R", "data.interactions.per.sample.R", "data.merged.interactions.R", "data.enhancers.R")){
+  objects=setdiff(ls, file)
+
+  rm(list=objects)
+
+  source(file)
+}
+
+#####################################################################
