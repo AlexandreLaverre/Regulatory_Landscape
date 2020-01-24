44c64a5e2b87df7ae844271d7e1053cfead5ace9
Alexandre Laverré <alexandre.laverre@univ-lyon1.fr>
2020-01-24 16:38:34 +0100

    added GC content calcul

--- sequence_alignment/extract_duplication_stats.py
+++ sequence_alignment/extract_duplication_stats.py
@@ -11,19 +11,20 @@ sp = sys.argv[1]
 treshold = sys.argv[2]  # ex : 0.8
 file = sys.argv[3]  # from ls : path + sp + "_restriction_fragments_mask/
 
-path = "/beegfs/data/alaverre/Regulatory_landscape/test/result/"+sp+"2other/duplication_rate/"
-output_file = path + file + "_blat_summary_treshold_" + treshold + ".txt"
+path = "/beegfs/data/alaverre/Regulatory_landscape/test/result/" + sp + "2other/duplication_rate/"
+output_file = path + file + "_blat_summary_treshold_" + treshold + ".txt2"
 seq_file = path + sp + "_restriction_fragments_mask/" + file
 BLAT_file = path + "output.psl/" + file + "_output.psl"
 
 # Counting N in frag
 seq_dict = SeqIO.to_dict(SeqIO.parse(seq_file, "fasta"))
-N_dict = {}
+content_dict = {}
 for frag in seq_dict.keys():
     sequence = seq_dict[frag]
     nb_N = sequence.seq.count("N")
+    nb_GC = sequence.seq.count("G") + sequence.seq.count("C")
     frag = frag.strip(':+')
-    N_dict[frag] = nb_N
+    content_dict[frag] = (nb_N, nb_GC)
 
 # Looking for available BLAT match
 dic_match = collections.defaultdict(dict)
@@ -35,9 +36,9 @@ with open(BLAT_file) as f1:
         frag = str(i[9]).strip(':+')
 
         length = int(i[10])
-        length_without_N = length - N_dict[frag]
+        length_without_N = length - content_dict[frag][0]
         dic_no_N[frag] = (
-        str(length), str(length_without_N))  # str(np.around((N_dict[frag]/length)*100, decimals=2))
+            str(length), str(length_without_N))  # str(np.around((N_dict[frag]/length)*100, decimals=2))
         match_part = int(i[0])  # np.around((int(i[0])/length_without_N)*100, decimals=2)
 
         match_ID = (str(i[13]) + ':' + str(i[15]) + ':' + str(i[16]))
@@ -51,7 +52,6 @@ with open(BLAT_file) as f1:
                 dic_match[frag][match_chr] = [[match_ID, match_part]]
             else:
                 dic_match[frag][match_chr].append([match_ID, match_part])
-
 
 ### Testing overlap between matches
 new_dic_match = {}
@@ -77,21 +77,20 @@ for frag in dic_match.keys():
                         end = new_end
 
                 else:
-                    collapse.append([str(chr_match+':'+start+':'+end), score])
+                    collapse.append([str(chr_match + ':' + start + ':' + end), score])
                     start = new_start
                     end = new_end
                     score = new_score
 
                 if i == len(dic_match[frag][chr_match]) - 1:
-                    collapse.append([str(chr_match+':'+start+':'+end), score])
+                    collapse.append([str(chr_match + ':' + start + ':' + end), score])
 
             dic_match[frag][chr_match] = collapse
 
         for match in dic_match[frag][chr_match]:
-            match[1] = float(int(match[1])/int(dic_no_N[frag][1]))*100
+            match[1] = float(int(match[1]) / int(dic_no_N[frag][1])) * 100
 
             new_dic_match[frag].append(tuple(match))
-
 
 # Sorting match dictionaries by score
 for frag in new_dic_match.keys():
@@ -101,11 +100,12 @@ for frag in new_dic_match.keys():
 # Writting output
 output = open(output_file, 'w')
 if os.stat(output_file).st_size == 0:
-    output.write("frag_ID\tlength\tno_N\tnb_match\tmatch\n")
+    output.write("frag_ID\tlength\tno_N\tnb_GC\tnb_match\tmatch\n")
 
 for frag in new_dic_match.keys():
     nb_match = len(new_dic_match[frag])
-    output.write(frag + "\t" + dic_no_N[frag][0] + "\t" + dic_no_N[frag][1] + "\t" + str(nb_match) + "\t")
+    output.write(frag + "\t" + dic_no_N[frag][0] + "\t" + dic_no_N[frag][1] + "\t" + content_dict[frag][1]
+                 + "\t" + str(nb_match) + "\t")
 
     count = 0
     for i in new_dic_match[frag]:
