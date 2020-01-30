6d2f338fb32f9ab5ccc6ea63bcedd9800a9be47b
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2020-01-30 15:41:28 +0100

    added script to do PCA on cell type

---
+++ expression_divergence/pca.cell.types.R
@@ -0,0 +1,42 @@
+####################################################################################
+
+pwd=getwd()
+dirs=unlist(strsplit(pwd, split="/"))
+path=paste(dirs[1:(length(dirs)-2)], collapse="/")
+path=paste(path, "/", sep="")
+
+####################################################################################
+
+pathExpression=paste(path, "results/expression_estimation/", sep="")
+pathOrtho=paste(path, "data/ensembl_ortho/", sep="")
+
+ensrelease=94
+
+####################################################################################
+
+exp.human=read.table(paste(pathExpression, "Human/AllSamples_KallistoNormalizedTPM_FilteredTranscripts.txt", sep=""), h=T, stringsAsFactors=F)
+exp.mouse=read.table(paste(pathExpression, "Mouse/AllSamples_KallistoNormalizedTPM_FilteredTranscripts.txt", sep=""), h=T, stringsAsFactors=F)
+
+rownames(exp.human)=exp.human$GeneID
+rownames(exp.mouse)=exp.mouse$GeneID
+
+exp.human=exp.human[,-1]
+exp.mouse=exp.mouse[,-1]
+
+colnames(exp.human)=paste("Human", colnames(exp.human), sep="_")
+colnames(exp.mouse)=paste("Mouse", colnames(exp.mouse), sep="_")
+
+####################################################################################
+
+ortho=read.table(paste(pathOrtho, "Ortho_Human_Mouse_Ensembl",ensrelease,".txt", sep=""), h=T, stringsAsFactors=F, sep="\t", quote="\"")
+colnames(ortho)=c("Human", "Mouse", "HomologyType", "dN", "dS")
+ortho=ortho[which(ortho$HomologyType=="ortholog_one2one" & !is.na(ortho$dN) & !is.na(ortho$dS)),]
+
+ortho=ortho[which(ortho$Human%in%rownames(exp.human) & ortho$Mouse%in%rownames(exp.mouse)),]
+  
+####################################################################################
+  
+library(ade4)
+
+####################################################################################
+  
