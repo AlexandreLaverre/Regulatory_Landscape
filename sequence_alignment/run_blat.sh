69d1b211080608bfae149ce8e381f027738347cb
Alexandre Laverré <alexandre.laverre@univ-lyon1.fr>
2020-02-21 17:50:32 +0100

    Correction on Snakefile & added old run_blat script

--- sequence_alignment/run_blat.sh
+++ sequence_alignment/run_blat.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 sp=$1
-if [ ${sp} = "human" ]; then genome="Homo_sapiens.GRCh38.dna_rm.primary_assembly.fa"; else genome="Mus_musculus.GRCm38.dna_rm.primary_assembly.fa"; fi
+seq=$2
 
-path_data="/beegfs/data/alaverre/data/genome_ref/"
-path_result="/beegfs/data/alaverre/result/genome_alignment/${sp}2other/duplication_rate/"
+if [ ${sp} = "human" ]; then genome="hg38_masked.fa"; else genome="mm10_masked.fa"; fi
 
-for file in `ls ${path_result}${sp}_restriction_fragments_mask/ | xargs -n 1 basename`
+path_data="/beegfs/data/alaverre/Regulatory_landscape/test/data/genomes_rm/"
+path_result="/beegfs/data/alaverre/Regulatory_landscape/test/result/${sp}2other/duplication_rate/"
+
+for file in `ls ${path_result}${sp}_restriction_fragments_mask/*${seq}* | xargs -n 1 basename`
 do
 echo "#!/bin/bash" > ${path_result}running/BLAT_${file}
 echo "#SBATCH -o ${path_result}running/std_output_BLAT${file}.txt" >> ${path_result}running/BLAT_${file}
 echo "#SBATCH -e ${path_result}running/std_error_BLAT${file}.txt" >> ${path_result}running/BLAT_${file}
 echo "source /beegfs/home/alaverre/.bashrc" >> ${path_result}running/BLAT_${file}
-echo "blat -out=blast9 -minIdentity=95 ${path_data}${genome} ${path_result}${sp}_restriction_fragments_mask/${file} ${path_result}${file}_output.psl" >> ${path_result}running/BLAT_${file}
-sbatch -p normal --time=40:00:00 -c 1 --mem=4G ${path_result}running/BLAT_${file}
+echo "blat -out=psl -minIdentity=95 ${path_data}${genome} ${path_result}${sp}_restriction_fragments_mask/${file} ${path_result}/output.psl/${file}_output.psl" >> ${path_result}running/BLAT_${file}
+sbatch -p normal --time=70:00:00 -c 1 --mem=4G ${path_result}running/BLAT_${file}
 done
