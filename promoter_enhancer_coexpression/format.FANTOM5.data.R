92fd39f73544784973f03140d80c99cf35956d1f
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2020-05-19 21:14:51 +0200

    write output in tab-delimited format

--- promoter_enhancer_coexpression/format.FANTOM5.data.R
+++ promoter_enhancer_coexpression/format.FANTOM5.data.R
@@ -7,15 +7,15 @@ pathFOCS=paste(path, "/data/FOCS/", sep="")
 
 ###########################################################################
 
-for(sp in c("Human", "Mouse")){
-  if(sp=="Human"){
+for(sp in c("human", "mouse")){
+  if(sp=="human"){
     load(paste(pathFOCS, sp, "/FANTOM5/fantom.enh.tpm.RData", sep=""))
     load(paste(pathFOCS, sp, "/FANTOM5/fantom.prom.tpm.RData", sep=""))
     version="hg19"
     
   }
 
-  if(sp=="Mouse"){
+  if(sp=="mouse"){
     load(paste(pathFOCS, sp, "/FANTOM5/fantom.enh.tpm.mm9.RData", sep=""))
     load(paste(pathFOCS, sp, "/FANTOM5/fantom.prom.tpm.mm9.RData", sep=""))
     version="mm9"
@@ -42,7 +42,24 @@ for(sp in c("Human", "Mouse")){
   results=data.frame("chr"=chr.promoters, "start"=start.promoters, "end"=end.promoters, "id"=id.promoters, "strand"=strand.promoters, stringsAsFactors=F)
 
   write.table(results, file=paste(pathFOCS, sp, "/FANTOM5/promoter_coordinates_",version,".bed", sep=""), row.names=F, col.names=F, sep="\t", quote=F)
+
+  ## write expression data now
+
+  if(dim(Me_fpkm)[2]!=dim(Mg_fpkm)[2]){
+    stop("weird! different column names")
+  }
+
+  if(!all(colnames(Me_fpkm)%in%colnames(Mg_fpkm))){
+    stop("weird! different column names")
+  }
+
+  Me_fpkm=Me_fpkm[,colnames(Mg_fpkm)]
   
+  write.table(Me_fpkm, file=paste(pathFOCS, sp, "/FANTOM5/enhancer_activity.txt", sep=""), row.names=T, col.names=T, sep="\t", quote=F)
+  
+  write.table(Mg_fpkm, file=paste(pathFOCS, sp, "/FANTOM5/promoter_activity.txt", sep=""), row.names=T, col.names=T, sep="\t", quote=F)
+  
+   
   ## cleanup
   rm("Me_fpkm")
   rm("Mg_fpkm")
