83e97cabc490a739ed7acdbf53827e9835789dad
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2020-06-05 14:48:07 +0200

    added expression stats

--- expression_statistics/evaluate.expression.specificity.R
+++ expression_statistics/evaluate.expression.specificity.R
@@ -1,6 +1,6 @@
 ############################################################################
 
-pathExpression="../../../RegulatoryLandscapeManuscript/SupplementaryDataset6b/"
+pathExpression="../../../RegulatoryLandscapesManuscript/SupplementaryDataset6b/"
 
 ############################################################################
 
@@ -26,8 +26,35 @@ filename=paste("RPKMValues_",dataset,".txt", sep="")
 
 for(sp in c("human", "mouse")){
 
-  exp=read.table(paste(pathExpression, sp, "/", filename, sep=""), h=T, stringsAsFactors=F, sep="\t", quote="\"")
+  print(sp)
 
+  exp=read.table(paste(pathExpression, sp, "/", filename, sep=""), h=T, stringsAsFactors=F, sep=" ", quote="\"")
+  samples=colnames(exp)
+
+  tissue=unlist(lapply(samples, function(x) unlist(strsplit(x, split="\\."))[1]))
+  age=unlist(lapply(samples, function(x) unlist(strsplit(x, split="\\."))[2]))
+
+  tissage=as.factor(paste(tissue, age, sep="."))
+
+  avgexp=t(apply(exp, 1, function(x) tapply(as.numeric(x), tissage, mean)))
+
+  avgexp=as.matrix(avgexp)
+  
+  tau.rpkm=apply(avgexp, 1, compute.tau)
+  tau.logrpkm=apply(log2(avgexp+1), 1, compute.tau)
+
+  maxexp=apply(avgexp, 1, max)
+  maxsample=apply(avgexp,1, function(x) colnames(avgexp)[which.max(x)])
+  maxsample[which(maxexp==0)]=NA
+
+  maxtiss=unlist(lapply(maxsample, function(x) unlist(strsplit(x, split="\\."))[1]))
+  maxage=unlist(lapply(maxsample, function(x) unlist(strsplit(x, split="\\."))[2]))
+
+  nbsamplesexp=apply(avgexp, 1, function(x) length(which(x>0)))
+
+  results=data.frame("GeneID"=rownames(avgexp), "TauRPKM"=tau.rpkm, "TauLogRPKM"=tau.logrpkm, "MaxRPKM"=maxexp, "MaxSample"=maxsample, "MaxTissue"=maxtiss, "MaxAge"=maxage, "NbSamplesDetectedExpression"=nbsamplesexp, stringsAsFactors=F)
+
+  write.table(results, file=paste(pathExpression, sp, "/ExpressionStatistics_",dataset,".txt", sep=""), row.names=F, col.names=T, sep="\t", quote=F)
 }
 
 ############################################################################
