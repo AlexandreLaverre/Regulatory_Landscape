f5ff02201391eed976bec4d2e7c8b6db4bb150fd
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2020-06-05 09:39:53 +0200

    show arrows is optional

--- genome_browser/plot.annotations.R
+++ genome_browser/plot.annotations.R
@@ -1,6 +1,6 @@
 #####################################################################
 
-plot.annotations.genes <- function(gene.coords, focus.gene, gene.biotypes="all", xlim, col.focus="navy", col.other="gray60", axis=T, axisunit=NA, axisside=1, cex.name=1){
+plot.annotations.genes <- function(gene.coords, focus.gene, gene.biotypes="all", xlim, col.focus="navy", col.other="gray60", axis=T, axisunit=NA, axisside=1, cex.name=1, show.arrows=T){
 
   if(!focus.gene%in%gene.coords$id){
     stop(paste("cannot find", focus.gene, "in annotations"))
@@ -62,15 +62,20 @@ plot.annotations.genes <- function(gene.coords, focus.gene, gene.biotypes="all",
     rect(this.start, ypos-height/2, this.end, ypos+height/2, col=this.col, border=this.col, xpd=NA)
 
     if(this.strand=="+" | this.strand=="1"){
-      segments(this.start, ypos+height/2, this.start, ypos+height/2+arrowheight, col=this.col)
-      arrows(this.start, ypos+height/2+arrowheight, this.start+arrowsize, ypos+height/2+arrowheight, length=arrowlength, xpd=NA, lwd=1.5, col=this.col)
+      if(show.arrows){
+        segments(this.start, ypos+height/2, this.start, ypos+height/2+arrowheight, col=this.col)
+        arrows(this.start, ypos+height/2+arrowheight, this.start+arrowsize, ypos+height/2+arrowheight, length=arrowlength, xpd=NA, lwd=1.5, col=this.col)
+      }
+      
       if(g==focus.gene){
         text(this.name, x=(this.start+this.end)/2, y=ypos+height*0.75, adj=c(0.5, 0), cex=cex.name, font=3)
       }
     } else{
       if(this.strand=="-" | this.strand=="-1"){
-        segments(this.end, ypos+height/2, this.end, ypos+height/2+arrowheight, col=this.col)
-        arrows(this.end, ypos+height/2+arrowheight, this.end-arrowsize, ypos+height/2+arrowheight, length=arrowlength, xpd=NA, lwd=1.5, col=this.col)
+        if(show.arrows){
+          segments(this.end, ypos+height/2, this.end, ypos+height/2+arrowheight, col=this.col)
+          arrows(this.end, ypos+height/2+arrowheight, this.end-arrowsize, ypos+height/2+arrowheight, length=arrowlength, xpd=NA, lwd=1.5, col=this.col)
+        }
 
          if(g==focus.gene){
            text(this.name, x=(this.start+this.end)/2, y=ypos-height*0.75, adj=c(0.5, 1), cex=cex.name, font=3)
