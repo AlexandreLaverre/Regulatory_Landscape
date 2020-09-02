4562da2188a594c668fc1a0a658c6bd981476990
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2020-09-02 17:16:41 +0300

    output for average expression per sample

--- expression_statistics/evaluate.expression.specificity.R
+++ expression_statistics/evaluate.expression.specificity.R
@@ -40,6 +40,8 @@ for(sp in c("human", "mouse")){
     avgexp=t(apply(exp, 1, function(x) tapply(as.numeric(x), tissage, mean)))
     
     avgexp=as.matrix(avgexp)
+
+    write.table(avgexp, file=paste(pathExpression, sp, "/AverageRPKM_",dataset,".txt", sep=""), row.names=T, col.names=T, sep="\t", quote=F)
     
     tau.rpkm=apply(avgexp, 1, compute.tau)
     tau.logrpkm=apply(log2(avgexp+1), 1, compute.tau)
