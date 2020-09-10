c228308d7ac0dd87d6a43c33a78df5ad4d3d3ba3
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2020-09-10 15:12:31 +0300

    corrected typo

--- main_figures/data.fragment.contacts.R
+++ main_figures/data.fragment.contacts.R
@@ -10,15 +10,15 @@ simulated.contacts=list()
 for(sp in c("human", "mouse")){
   obs <- read.table(paste(pathFinalData, "SupplementaryDataset1/", sp, "/all_interactions.txt", sep=""), header=T)
   simul <- read.table(paste(pathFinalData, "SupplementaryDataset2/", sp, "/simulated_all_interactions.txt", sep=""), header=T)
-
+  
   ## select interactions in cis
   obs=obs[which(obs$chr_bait==obs$chr),]
   sim=sim[which(sim$chr_bait==sim$chr),]
-    
+  
   ## select bait-other interactions
   obs=obs[which(obs$type=="unbaited"),]
   sim=sim[which(sim$type=="unbaited"),]
-
+  
   ## select interactions in the accepted distance range
   obs=obs[which(obs$distance>=minDistance & obs$distance<=maxDistance),]
   sim=sim[which(sim$distance>=minDistance & sim$distance<=maxDistance),]
@@ -29,7 +29,7 @@ for(sp in c("human", "mouse")){
 
 #################################################################################
 
-save(list=c("observed.contacts", "simulated.contacts"), file=paste(pathFigures, "RData/data.fragment.contacts.RData",sep="")
+save(list=c("observed.contacts", "simulated.contacts"), file=paste(pathFigures, "RData/data.fragment.contacts.RData",sep=""))
 
 #################################################################################
 
