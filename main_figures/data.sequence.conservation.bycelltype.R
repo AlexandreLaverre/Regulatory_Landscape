d3fc96e8010dc8b357b17fc307dcad69c61d1437
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2020-10-31 21:26:06 +0100

    format

--- main_figures/data.sequence.conservation.bycelltype.R
+++ main_figures/data.sequence.conservation.bycelltype.R
@@ -61,8 +61,8 @@ for(ref_sp in c("human", "mouse")){
     
     path_evol <- paste(pathFinalData, "SupplementaryDataset7/", ref_sp, "/", sep="")
     path_annot <- paste(pathFinalData, "SupplementaryDataset4/", ref_sp, "/", sep="")
-        
-    ### statistics for the contacted restriction fragments
+    
+### statistics for the contacted restriction fragments
 
     obs <- read.table(paste(pathFinalData, "SupplementaryDataset5/", ref_sp, "/statistics_contacted_sequence_original.txt", sep=""), header=T)
     simul <- read.table(paste(pathFinalData, "SupplementaryDataset5/", ref_sp,"/statistics_contacted_sequence_simulated.txt", sep=""), header=T)
@@ -73,7 +73,7 @@ for(ref_sp in c("human", "mouse")){
     rownames(obs) <- obs$ID
     rownames(simul) <- simul$ID
     
-    # unbaited only
+                                        # unbaited only
     obs <- obs[which(obs$baited == "unbaited"),]
     simul <- simul[which(simul$baited == "unbaited"),]
 
