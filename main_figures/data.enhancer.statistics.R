bdf732628900e4c9b6338cfd2a3f169472a8e3d7
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2020-11-05 22:46:48 +0100

    added enhancer filtering

--- main_figures/data.enhancer.statistics.R
+++ main_figures/data.enhancer.statistics.R
@@ -1,8 +1,10 @@
 ###########################################################################
 
+library(data.table)
+
 source("parameters.R")
 
-pathContacts=paste(pathFinalData, "SupplementaryDataset4/", sep="")
+pathStats=paste(pathFinalData, "SupplementaryDataset4/", sep="")
 
 ###########################################################################
 
@@ -14,10 +16,29 @@ for(sp in c("human", "mouse")){
   
   for(enh in enhancer.datasets[[sp]]){
     
-    real=read.table(paste(pathContacts, sp, "/", enh, "/statistics_contacted_enhancers_original.txt", sep=""), h=T, stringsAsFactors=F, sep="\t", quote="\"")
-    sim=read.table(paste(pathContacts, sp, "/", enh, "/statistics_contacted_enhancers_simulated.txt", sep=""), h=T, stringsAsFactors=F, sep="\t", quote="\"")
+    obs=fread(paste(pathStats, sp, "/", enh, "/statistics_contacted_enhancers_original.txt", sep=""), h=T, stringsAsFactors=F, sep="\t", quote="\"")
+    sim=fread(paste(pathStats, sp, "/", enh, "/statistics_contacted_enhancers_simulated.txt", sep=""), h=T, stringsAsFactors=F, sep="\t", quote="\"")
+
+    class(obs)<-"data.frame"
+    class(sim)<-"data.frame"
+
+    ## create enhancer identifier
     
-    enhancer.statistics[[sp]][[enh]]=list("real"=real, "simulated"=sim)
+    obs$enh <-  do.call(paste,c(obs[c("chr","start","end")],sep=":"))
+    sim$enh <-  do.call(paste,c(sim[c("chr","start","end")],sep=":"))
+
+    ## rownames
+    rownames(obs) <- obs[,"enh"]
+    rownames(sim) <- sim[,"enh"]
+    
+    ## select enhancers that are not duplicated and with repeat_part < 1%
+    
+    obs <- obs[which(obs$BLAT_match < 2 & (obs$repeat_bp/obs$length) < 0.01),] 
+    sim <- sim[which(sim$BLAT_match < 2 & (sim$repeat_bp/sim$length) < 0.01),]
+    
+    ## save results
+    
+    enhancer.statistics[[sp]][[enh]]=list("original"=obs, "simulated"=sim)
   }
 }
 
