100398c59551ad9a7177ba2a377915813db36fae
Alexandre Laverré <alexandre.laverre@univ-lyon1.fr>
2020-12-02 19:29:53 +0100

    cleaning

--- main_figures/parameters.R
+++ main_figures/parameters.R
@@ -56,13 +56,7 @@ names(col.enhancers)=c("ENCODE", "FANTOM5", "FOCS_GRO_seq", "RoadmapEpigenomics"
 
 
 dataset.colors=c("darkorange", "dodgerblue4") ##c("forestgreen", "firebrick1")
-# dataset.density=c(10,10)
-# dataset.angle=c(45,-45)
-
 names(dataset.colors)=c("Original", "Simulated")
-# names(dataset.density)=c("Original", "Simulated")
-# names(dataset.angle)=c("Original", "Simulated")
-
 col.Shh=dataset.colors["Original"]
 
 #######################################################################################
