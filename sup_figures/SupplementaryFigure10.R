b0107cbad0d471c9efce732d5c79f8a819d2ed79
Alexandre Laverré <alexandre.laverre@univ-lyon1.fr>
2020-12-02 19:32:18 +0100

    Changing boxplot to points and Adding Simulated data

--- sup_figures/SupplementaryFigure10.R
+++ sup_figures/SupplementaryFigure10.R
@@ -24,7 +24,7 @@ if(load){
   maxval=c(7, 5)
   names(maxval)=c("human", "mouse")
 
-  labels=c("A", "B")
+  labels=c("a", "b")
   names(labels)=c("human", "mouse")
   
   load=FALSE
@@ -52,11 +52,11 @@ if(prepare){
     
     obs$nb_celltypes <- apply(obs[,samples],1, function(x) length(unique(celltypes[which(!is.na(x))])))
     obs$celltype_class<- cut(obs$nb_celltypes, breaks=c(0:M, max(obs$nb_celltypes)), include.lowest=T)
-    levels(obs$celltype_class)=c(as.character(1:M), paste(M+1, "+", sep=""))
+    levels(obs$celltype_class)=c(as.character(1:M), paste0(">",M))
 
     sim$nb_celltypes <- apply(sim[,samples],1, function(x) length(unique(celltypes[which(!is.na(x))])))
     sim$celltype_class<- cut(sim$nb_celltypes, breaks=c(0:M, max(sim$nb_celltypes)), include.lowest=T)
-    levels(sim$celltype_class)=c(as.character(1:M), paste(M+1, "+", sep=""))
+    levels(sim$celltype_class)=c(as.character(1:M), paste0(">",M))
 
     ## bait annotation
 
@@ -95,7 +95,7 @@ if(prepare){
 
 pdf(paste(pathFigures, "SupplementaryFigure10.pdf", sep=""), width=6.85, height=3.5)
 
-m=matrix(c(rep(1, 8), rep(2, 6)), nrow=1)
+m=matrix(c(rep(1, 7), rep(2, 6)), nrow=1)
 
 layout(m)
 
@@ -113,8 +113,7 @@ for(sp in c("human", "mouse")){
 
   xlim=c(0.5, nbclass+0.5)
   
-  ylim=c(0, max(c(obs$NbSamplesExpressed, sim$NbSamplesExpressed)))
-  ylim[2]=ylim[2]+diff(ylim)/20
+  if (sp == "human"){ylim=c(70,120)}else{ylim=c(50,100)}
 
   par(mar=c(3.5, 3.5, 1.5, 1.1))
       
@@ -122,7 +121,17 @@ for(sp in c("human", "mouse")){
   
   for(i in 1:nbclass){
     wobs=which(obs$celltype_class==classes[i])
-    boxplot(obs$NbSamplesExpressed[wobs], outline=F, add=T, axes=F, at=xpos[i], border=dataset.colors["Original"], notch=T)
+    obs_stats = boxplot(obs$NbSamplesExpressed[wobs], plot=F)
+    
+    wsim=which(sim$celltype_class==classes[i])
+    sim_stats= boxplot(sim$NbSamplesExpressed[wobs], plot=F)
+    
+    
+    points(i, obs_stats$stats[3,], pch=20, col=dataset.colors["Original"])
+    segments(i, obs_stats$conf[1,], i, obs_stats$conf[2,], col=dataset.colors["Original"])
+    
+    points(i, sim_stats$stats[3,], pch=20, col=dataset.colors["Simulated"])
+    segments(i, sim_stats$conf[1,], i, sim_stats$conf[2,], col=dataset.colors["Simulated"])
   }
 
   mtext(sp, side=3, cex=0.75)
@@ -134,6 +143,9 @@ for(sp in c("human", "mouse")){
   mtext("number of samples with detectable expression", side=2, line=2, cex=0.7)
 
   mtext(labels[sp], side=3, at=xlim[1]-diff(xlim)/(nbclass-0.8), line=0, font=2)
+  
+  if (sp == "human"){legend("topleft", legend=c("PCHi-C data", "simulated data"), col=dataset.colors[c("Original", "Simulated")], pch=20,
+                            bty='n', inset=c(0.01, 0.05), xpd=NA)}
 }
 
 ##########################################################################
