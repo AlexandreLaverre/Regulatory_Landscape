038ede14dd299c04bf7850d5625fcc9c6d06948a
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2021-01-15 00:01:07 +0200

    figure 5'

--- main_figures/tmp.fig5.R
+++ main_figures/tmp.fig5.R
@@ -19,7 +19,7 @@ if(load){
   
   load(paste(pathFigures, "RData/data.sample.info.RData", sep=""))
   load(paste(pathFigures, "RData/data.contact.conservation.enhancers.RData", sep=""))
-  load(paste(pathFigures, "RData/data.contact.conservation.enhancers.", ref, ".stats.RData", sep=""))
+  load(paste(pathFigures, "RData/data.contact.conservation.enhancers.", ref, ".stats.Rdata", sep=""))
 
   enh="ENCODE"
     
@@ -29,61 +29,32 @@ if(load){
 ##############################################################################
 
 if(prepare){
-
-  samples.ref=sampleinfo[[ref]][,"Sample.ID"]
-  samples.tg=sampleinfo[[tg]][,"Sample.ID"]
-
-  cells.ref=sampleinfo[[ref]][,"Broad.cell.type.or.tissue"]
-  cells.tg=sampleinfo[[tg]][,"Broad.cell.type.or.tissue"]
-
-  names(cells.ref)=samples.ref
-  names(cells.tg)=samples.tg
-
-  nbmax=length(unique(cells.tg))
-
-  ## observed and simulated contacts
-    
-  cc.obs=contact.conservation[[paste(ref, "2", tg, sep="")]][[enh]][["obsobs"]]
-  cc.sim=contact.conservation[[paste(ref, "2", tg, sep="")]][[enh]][["simsim"]]
-
-  cc.obs$target_nb_cell=apply(cc.obs[,samples.tg],1, function(x) length(unique(cells.tg[which(x>0)])))
-  cc.sim$target_nb_cell=apply(cc.sim[,samples.tg],1, function(x) length(unique(cells.tg[which(x>0)])))
   
-  cc.obs$ref_nb_cell=apply(cc.obs[,samples.ref],1, function(x) length(unique(cells.ref[which(x>0)])))
-  cc.sim$ref_nb_cell=apply(cc.sim[,samples.ref],1, function(x) length(unique(cells.ref[which(x>0)])))
+  sampleinfo.ref=sampleinfo[[ref]]
+  sampleinfo.tg=sampleinfo[[tg]]
 
-  ## conservation by distance class
+  rownames(sampleinfo.ref)=sampleinfo.ref$Sample.ID
+  rownames(sampleinfo.tg)=sampleinfo.tg$Sample.ID
 
-  cc.obs$dist_class=cut(cc.obs$origin_dist, breaks=seq(from=minDistance, to=maxDistance, by=50000), include.lowest=T)
-  cc.sim$dist_class=cut(cc.sim$origin_dist, breaks=seq(from=minDistance, to=maxDistance, by=50000), include.lowest=T)
+  ## excess of contact conservation by sample
 
-  prop.obs.dist=100*tapply(cc.obs$target_nb_cell, cc.obs$dist_class, function(x) length(which(x>0))/length(x))
-  prop.sim.dist=100*tapply(cc.sim$target_nb_cell, cc.sim$dist_class, function(x) length(which(x>0))/length(x))
+  mat.cons.obs=100*matrix.cons.obs[[enh]]
+  mat.cons.sim=100*matrix.cons.sim[[enh]]
+  
+  diffmat=mat.cons.obs-mat.cons.sim
 
-  ci.low.obs.dist=100*tapply(cc.obs$target_nb_cell, cc.obs$dist_class, function(x) prop.test(length(which(x>0)), length(x))$conf.int[1])
-  ci.low.sim.dist=100*tapply(cc.sim$target_nb_cell, cc.sim$dist_class, function(x) prop.test(length(which(x>0)), length(x))$conf.int[1])
+  s.ref=rep(rownames(diffmat), dim(diffmat)[2])
+  s.tg=rep(colnames(diffmat), each=dim(diffmat)[1])
 
-  ci.high.obs.dist=100*tapply(cc.obs$target_nb_cell, cc.obs$dist_class, function(x) prop.test(length(which(x>0)), length(x))$conf.int[2])
-  ci.high.sim.dist=100*tapply(cc.sim$target_nb_cell, cc.sim$dist_class, function(x) prop.test(length(which(x>0)), length(x))$conf.int[2])
+  diffmat.table=data.frame("diff"=as.numeric(diffmat), "sample.ref"=s.ref, "sample.tg"=s.tg, "cell.ref"=sampleinfo.ref[s.ref,"Broad.cell.type.or.tissue"], "cell.tg"=sampleinfo.tg[s.tg,"Broad.cell.type.or.tissue"])
 
-  ## conservation by number of cell types
+  diffmat=t(diffmat)
 
-  nbmax=max(cc.obs$ref_nb_cell)
-  cc.obs$cell_class=cut(cc.obs$ref_nb_cell, breaks=c(seq(from=0, to=5, by=1), nbmax), include.lowest=T)
-  cc.sim$cell_class=cut(cc.sim$ref_nb_cell, breaks=c(seq(from=0, to=5, by=1), nbmax), include.lowest=T)
+  ## conservation by distance
+  cons.dist = cons.dist[["all"]]
+  cons.dist.conf.low = cons.dist.conf.low[["all"]]
+  cons.dist.conf.high = cons.dist.conf.high[["all"]]
 
-  prop.obs.cell=100*tapply(cc.obs$target_nb_cell, cc.obs$cell_class, function(x) length(which(x>0))/length(x))
-  prop.sim.cell=100*tapply(cc.sim$target_nb_cell, cc.sim$cell_class, function(x) length(which(x>0))/length(x))
-
-  ci.low.obs.cell=100*tapply(cc.obs$target_nb_cell, cc.obs$cell_class, function(x) prop.test(length(which(x>0)), length(x))$conf.int[1])
-  ci.low.sim.cell=100*tapply(cc.sim$target_nb_cell, cc.sim$cell_class, function(x) prop.test(length(which(x>0)), length(x))$conf.int[1])
-
-  ci.high.obs.cell=100*tapply(cc.obs$target_nb_cell, cc.obs$cell_class, function(x) prop.test(length(which(x>0)), length(x))$conf.int[2])
-  ci.high.sim.cell=100*tapply(cc.sim$target_nb_cell, cc.sim$cell_class, function(x) prop.test(length(which(x>0)), length(x))$conf.int[2])
-
-  matrix.cell=matrix(c(prop.obs.cell, prop.sim.cell), nrow=2, byrow=T)
-
-  ## conservation in comparable cell types
   
   prepare=FALSE
 }
@@ -97,86 +68,173 @@ if(prepare){
 
 ##############################################################################
 
-pdf(paste(pathFigures, "Figure5.pdf", sep=""), width=6.85, height=2.5)
+pdf(paste(pathFigures, "Figure5.pdf", sep=""), width=6.85, height=4.5)
 
-m=matrix(rep(NA, 1*13), nrow=1)
-m[1,]=c(rep(1,4), rep(2,3), rep(3,6))
+m=matrix(rep(NA, 10*20), nrow=10)
+
+for(i in 1:5){
+  m[i,]=c(rep(1,10), rep(2, 10))
+}
+
+for(i in 6:10){
+  m[i,]=c(rep(1,10), rep(3, 10))
+}
+
+
+
 layout(m)
 
-mtext.CEX=0.75
-par(lwd = 1.5)
+mtext.CEX=0.7
 
-#########################################################################################
+##############################################################################
 
-## conservation as a function of nb cell types
+## heatmap for excess of contact conservation by cell type
 
-par(mar=c(3.1, 4.5, 2.5, 0.25))
-b=barplot(matrix.cell, border=dataset.colors[c("Original", "Simulated")],  col=dataset.colors[c("Original", "Simulated")], mgp=c(3, 0.75, 0), las=2, beside=T, cex.axis=1.05, ylim=c(0, 52), space=c(0.2, 1))
+zmax=max(abs(as.numeric(diffmat)))
+zlim=c(-zmax, zmax)   ## range(as.numeric(diffmat))
 
-segments(b[1,], ci.low.obs.cell, b[1,], ci.high.obs.cell)
-segments(b[2,], ci.low.sim.cell, b[2,], ci.high.sim.cell)
+par(mar=c(12.1, 11.1, 2.1, 1.1))
 
-axis(side=1, at=apply(b,2,mean), labels=c(as.character(1:5), "6+"), cex.axis=1.05, mgp=c(3, 0.75, 0))
-mtext("% conserved contacts", side=2, line=3, cex=mtext.CEX)
-mtext("nb. cell types", side=1, line=2.0, cex=mtext.CEX)
+nbcol=50
 
-mtext("a", side=3, line=1.25, at=-5.65, font=2, cex=1.05)
+col.heatmap=colorRampPalette(c("darkred", "white", "navy"), space="rgb")(nbcol)
 
-## legend 
-legend("topleft", legend=c("PCHi-C data", "simulated data"), border=dataset.colors[c("Original", "Simulated")],
-       fill=dataset.colors[c("Original", "Simulated")], bty='n', 
-       inset=c(0.05, -0.12), xpd=NA)
+image(diffmat, zlim=zlim, axes=F, col=col.heatmap)
 
-####################################################################################
+xpos=seq(from=0, to=1, length=dim(diffmat)[1])
+names(xpos)=rownames(diffmat)
 
-## conservation in common cell types
+ypos=seq(from=0, to=1, length=dim(diffmat)[2])
+names(ypos)=colnames(diffmat)
 
-YMAX=40
+smally=diff(ypos)[1]/2
+smallx=diff(xpos)[1]/2
 
-par(mar=c(4.1, 4.5, 2.5, 1.5))
+tinyx=diff(xpos)[1]/10
+tinyy=diff(ypos)[1]/10
 
-b=barplot(cons.common.cell[[enh]], beside=T, names=rep("", dim(cons.common.cell[[enh]])[2]), ylim=c(0,YMAX), space=c(0.2,1),
-          border=dataset.colors[c("Original", "Simulated")],  col=dataset.colors[c("Original", "Simulated")],
-          mgp=c(3, 0.75, 0), cex.axis=1.05, las=2)
+for(c in unique(sampleinfo.ref$Broad.cell.type.or.tissue)){
+  s=sampleinfo.ref$Sample.ID[which(sampleinfo.ref$Broad.cell.type.or.tissue==c)]
+  this.y=ypos[s]
+  mtext(syn.celltypes[c], side=2, line=0.75, cex=0.7, las=2, at=mean(this.y))
 
-arrows(x0=b,y0=cons.common.cell.conf.low[[enh]],y1=cons.common.cell.conf.high[[enh]],angle=90,code=3,length=0.01)
+  segments(-0.075, min(this.y)-tinyy,  -0.075, max(this.y)+tinyy, xpd=NA)
+}
 
-## axis labels
+for(c in unique(sampleinfo.tg$Broad.cell.type.or.tissue)){
+  s=sampleinfo.tg$Sample.ID[which(sampleinfo.tg$Broad.cell.type.or.tissue==c)]
+  this.x=xpos[s]
+  mtext(syn.celltypes[c], side=1, line=0.75, cex=0.7, las=2, at=mean(this.x))
 
-mtext("% conserved contacts", side=2, line=2.5, cex=mtext.CEX)
+  segments(min(this.x)-tinyx, -0.04,  max(this.x)+tinyx,  -0.04, xpd=NA)
+}
 
-label.cells = c("ESC", "adipo", "B cells")
-mtext(label.cells, at=apply(b, 2, mean), side=1, line=0.5, cex=mtext.CEX, las=2)
+for(c in c("pre-adipocytes", "embryonic stem cells", "B lymphocytes")){
+  sr=sampleinfo.ref$Sample.ID[which(sampleinfo.ref$Broad.cell.type.or.tissue==c)]
+  this.y=ypos[sr]
 
+  st=sampleinfo.tg$Sample.ID[which(sampleinfo.tg$Broad.cell.type.or.tissue==c)]
+  this.x=xpos[st]
 
-mtext("b", side=3, line=1, at=-4, font=2, cex=1.05)
+  rect(min(this.x)-smallx, min(this.y)-smally, max(this.x)+smallx, max(this.y)+smally, border="gold", xpd=NA)
+}
 
-#########################################################################################
+## legend for the heatmap
 
-## conservation as a function of distance
+startx=seq(from=-0.7, to=-0.3, length=nbcol)
+step=diff(startx)[1]
+endx=startx+step
 
-xpos=1:length(prop.obs.dist)
-xlim=c(-1, max(xpos)+1)
-ylim=c(-0.5, 40)
+starty=rep(-0.3, 1)
+endy=rep(-0.25, 1)
+tty=(endy-starty)/4.5
+sy=(endy-starty)/1.25
 
-par(mar=c(4.1, 4.5, 2.5, 1.5))
+rect(startx, starty, endx, endy, col=col.heatmap, border=col.heatmap, xpd=NA)
+
+segments(min(startx), starty-tty, max(endx), starty-tty, xpd=NA)
+segments(min(startx), starty-sy,  min(startx), starty-tty, xpd=NA)
+segments(max(endx), starty-sy,  max(endx), starty-tty, xpd=NA)
+segments(mean(c(startx,endx)), starty-sy,  mean(c(startx,endx)), starty-tty, xpd=NA)
+
+text(round(zlim[1], digits=0), x=min(startx), y=starty-1.75*sy, xpd=NA, cex=0.95)
+text(round(zlim[2], digits=0), x=max(endx), y=starty-1.75*sy, xpd=NA, cex=0.95)
+text("0.0", x=mean(c(startx,endx)), y=starty-1.75*sy, xpd=NA, cex=0.95)
+
+text("% contact conservation", x=mean(c(startx,endx)), y=starty-3.2*sy, xpd=NA, cex=1.07)
+text("(observed-simulated)", x=mean(c(startx,endx)), y=starty-4.6*sy, xpd=NA, cex=1.07)
+
+mtext("a", side=3, font=2, line=0.75, at=-0.85, cex=1)
+
+##############################################################################
+
+## conservation as a number of cell types
+
+max.nb.cell = 8
+
+ylim=c(0, 55)
+xlim=c(0.5, max.nb.cell+0.5)
+xpos=seq(1, max.nb.cell, 1)
+names(xpos) = 1:max.nb.cell
+
+par(mar=c(3.5, 4.5, 2, 2.1))
 plot(1, type="n", xlab="", ylab="", axes=F, xlim=xlim, ylim=ylim, xaxs="i", yaxs="i")
 
-points(xpos, prop.obs.dist, col=dataset.colors["Original"], pch=20)
-segments(xpos, ci.low.obs.dist, xpos, ci.high.obs.dist, col=dataset.colors["Original"])
+for(nb_cell in 1:max.nb.cell){
+  x=xpos[nb_cell]
 
-points(xpos, prop.sim.dist, col=dataset.colors["Simulated"], pch=20)
-segments(xpos, ci.low.sim.dist, xpos, ci.high.sim.dist, col=dataset.colors["Simulated"])
+  points(x, cons.nb.cell[[enh]]["obs",nb_cell], pch=20, col=dataset.colors["Original"])
+  segments(x, cons.nb.cell.conf.low[[enh]]["obs", nb_cell], x, cons.nb.cell.conf.high[[enh]]["obs", nb_cell], col=dataset.colors["Original"])
 
-axis(side=1, at=c(xpos[1], xpos[10], xpos[20], xpos[30], xpos[39]+1), labels=c(0.025, 0.5, 1, 1.5, 2), cex.axis=1.05)
-axis(side=2, cex.axis=1.05, las=2)
-mtext("% conserved contacts", side=2, line=3, cex=mtext.CEX)
-mtext("distance to promoters (Mb)", side=1, line=2.5, cex=mtext.CEX)
+  points(x, cons.nb.cell[[enh]]["sim",nb_cell], pch=20, col=dataset.colors["Simulated"])
+  segments(x, cons.nb.cell.conf.low[[enh]]["sim", nb_cell], x, cons.nb.cell.conf.high[[enh]]["sim", nb_cell], col=dataset.colors["Simulated"])
+}
 
-mtext("c", side=3, line=1.25, at=-9.1, font=2, cex=1.05)
+abline(v=xpos[1:max.nb.cell-1]+0.5, lty=3, col="gray40")
 
-#########################################################################################
+axis(side=1, at=xpos, mgp=c(3, 0.5, 0), labels=rep("", max.nb.cell), mgp=c(3, 0.5, 0))
+mtext(colnames(cons.nb.cell[[enh]]), at=xpos, side=1, line=0.5, cex=mtext.CEX)
+mtext("number of cell types", side=1, line=2, cex=mtext.CEX)
+
+axis(side=2, mgp=c(3, 0.75, 0), las=2)
+mtext("% conserved contacts", side=2, line=2.5, cex=mtext.CEX)
+
+legend("topleft", col=dataset.colors, legend = c("PCHi-C data", "simulated data"), box.col="white", bg="white", pch=20, inset=c(0.01, -0.05))
+
+mtext("b", side=3, line=0.5, at=-0.7, font=2, cex=1)
+
+##############################################################################
+
+YLIM=c(-1,30)
+
+class_leg <- c("0",  "0.5",  "1", "1.5", "2")
+
+par(lwd = 0.7)
+
+xlim=c(0.5, length(cons.dist[["ENCODE"]]["obs",])+0.5)
+
+par(mar=c(3.5, 4.5, 2, 2.1))
+plot(1, type="n", xlab="", ylab="", axes=F, xlim=xlim, ylim=YLIM,  yaxs="i")
+nbclass=length(cons.dist.conf.low[[enh]]["obs",])
+  
+points(cons.dist[[enh]]["obs",],pch=20, col=dataset.colors["Original"])
+segments(1:nbclass, cons.dist.conf.low[[enh]]["obs",], 1:nbclass, cons.dist.conf.high[[enh]]["obs",], col=dataset.colors["Original"])
+
+points(cons.dist[[enh]]["sim",],pch=20, col=dataset.colors["Simulated"])
+segments(1:nbclass, cons.dist.conf.low[[enh]]["sim",], 1:nbclass, cons.dist.conf.high[[enh]]["sim",], col=dataset.colors["Simulated"])
+
+## axis, legend & plot label
+axis(side=1, at=c(1,10,20,30,40), labels=class_leg, mgp=c(3, 0.65, 0))
+mtext("distance from promoter region (Mb)", side=1, line=2, cex=mtext.CEX)
+
+axis(side=2, mgp=c(3, 0.75, 0), las=2)
+mtext("% conserved contacts", side=2, line=2.5,  cex=mtext.CEX)
+
+mtext("c", side=3, line=1.75, at=-7.5, font=2, cex=1.05)
+
+##############################################################################
 
 dev.off()
 
-####################################################################################
+##############################################################################
+##############################################################################
