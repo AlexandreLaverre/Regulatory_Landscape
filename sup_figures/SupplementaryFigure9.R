1e2c6939f64eb975dbe7c406ad41400f02f8ef51
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2021-01-25 09:49:07 +0100

    corrected x axis labels

--- sup_figures/SupplementaryFigure9.R
+++ sup_figures/SupplementaryFigure9.R
@@ -43,19 +43,21 @@ for (sp in c("human", "mouse")){
     segments(xpos, enh_prop_dist[["simul"]][[paste0(enh,"_conflow")]], xpos, enh_prop_dist[["simul"]][[paste0(enh,"_confup")]], col=dataset.colors["Simulated"])
     
     
-    class_leg <- c("0", "0.5", "1", "1.5", "2")
+    class_leg <- c("0.05", "0.5", "1", "1.5", "2") ## first class has an average of 50kb
+    
     axis(side=1, at=c(1,10,20,30,40), labels=class_leg, mgp=c(3, 0.65, 0), cex.axis=1.1)
     axis(side=2, mgp=c(3, 0.65, 0), cex.axis=1.1, las=2)
     
     if (nb == 1){
       legend("topright", legend=c("PCHi-C data", "simulated data"), 
-             col=dataset.colors[c("Original", "Simulated")],lty=1, seg.len=1, bty='n', cex=1, inset=c(0.05, 0.05), xpd=NA)}
+             col=dataset.colors[c("Original", "Simulated")], lty=1, seg.len=1, bty='n', cex=1.1, inset=c(0.05, 0.05), xpd=NA)
+    }
     
     
     mtext("% length covered\n by enhancers", side=2, cex=0.85, line=2, at=(ymin+ymax)*0.9/2)
     mtext("distance to promoters (Mb)", side=1, line=2, cex=0.85)
     
-    mtext(letters[nb], side=3, line=1.45, at=-5.75, font=2, cex=1.2)
+    mtext(letters[nb], side=3, line=1, at=-7.75, font=2, cex=1.2)
     mtext(paste(sp, enh, sep=" "), side=3, cex=0.8, line=0)
     
     nb = nb+1
