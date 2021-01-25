4ca6716bfa5a8570489fa3a66fc14311a788af3a
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2021-01-25 10:17:48 +0100

    removed duplicated panel, changed cex sizes

--- sup_figures/SupplementaryFigure11.R
+++ sup_figures/SupplementaryFigure11.R
@@ -17,7 +17,7 @@ if(load){
   
   load(paste(pathFigures, "RData/data.promoter.enhancer.correlation.", ref_sp, ".Rdata", sep=""))
   
-  enhancers = enhancer.datasets[[ref_sp]]
+  enhancers = setdiff(enhancer.datasets[[ref_sp]], "ENCODE") ## ENCODE shown in main figure
 }
 
 #################################################################################################################
@@ -29,11 +29,15 @@ if(load){
 
 #################################################################################################################
 
-pdf(paste(pathFigures, "SupplementaryFigure11.pdf", sep=""), width=6.85, height=6)
+pdf(paste(pathFigures, "SupplementaryFigure11.pdf", sep=""), width=3.4, height=6.85)
 
 ############################################################################################################################
 ############################################## correlation gene expression and enhancers activity ##########################
-par(mfrow=c(2,2))
+
+par(mfrow=c(3,1))
+
+labels=c("a", "b", "c")
+names(labels)=enhancers
 
 for (enh in enhancers){
   ymin=min(c(correl_activity[["obs"]][[paste0(enh,"_conflow")]], correl_activity[["obs"]][[paste0(enh,"_confup")]],  correl_activity[["simul"]][[paste0(enh,"_conflow")]], correl_activity[["simul"]][[paste0(enh,"_confup")]]))
@@ -55,20 +59,21 @@ for (enh in enhancers){
   
   
   
-  class_leg <- c("0", "0.5", "1", "1.5", "2")
+  class_leg <- c("0.05", "0.5", "1", "1.5", "2") ## first class is from 25kb to 75kb
   axis(side=1, at=c(1,10,20,30,40), labels=class_leg, mgp=c(3, 0.65, 0), cex.axis=1.1)
   axis(side=2, mgp=c(3, 0.65, 0), cex.axis=1.1, las=2)
   
-  mtext("Spearman's rho", side=2, line=3, cex=1.2)
-  mtext("distance to promoters (Mb)", side=1, line=2, cex=1.2)
+  mtext("Spearman's rho", side=2, line=3, cex=0.8)
+  mtext("distance to promoters (Mb)", side=1, line=2, cex=0.8)
   
-  mtext(enh.syn[[enh]], side=3, line=0, cex=1.2)
+  mtext(enh.syn[[enh]], side=3, line=0, cex=0.8)
   
   if (enh == "FANTOM5"){
     legend("topright", legend=c("PCHi-C data", "simulated data"), col=dataset.colors[c("Original", "Simulated")],lty=1, seg.len=1, bty='n', cex=1.1, inset=c(0.05, 0.1), xpd=NA)
     
   }
-  
+
+  mtext(labels[enh], side=3, line=1, at=-7.7, font=2, cex=1.2)
 }
 
 ##################################################################################################################################
