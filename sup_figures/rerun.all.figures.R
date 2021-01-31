0134559aa42674302612c94671963da3799b442b
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2021-01-31 19:43:49 +0100

    removed unused fitler in rerun.all.figures

--- sup_figures/rerun.all.figures.R
+++ sup_figures/rerun.all.figures.R
@@ -1,10 +1,8 @@
 
 ## all scripts that generate figures
 
-
 all.files=system("ls", intern=T)
 fig.scripts=c(grep("SupplementaryFigure", all.files, value=T), grep("ExtendedFigure", all.files, value=T))
-fig.scripts=grep("SupplementaryFigureX", fig.scripts, value=T, invert=T)
 
 ############################################################################
 
