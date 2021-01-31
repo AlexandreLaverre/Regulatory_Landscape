769d21563e1e5a5f391152201af0361f23278b56
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2021-01-31 19:41:29 +0100

    ext fig numbering

--- sup_figures/ExtendedFigure5.R
+++ sup_figures/ExtendedFigure5.R
@@ -1,120 +1,145 @@
 ######################################################################################################################
-
 library(Hmisc)
-
-source("../main_figures/parameters.R")
-
-######################################################################################################################
+options(stringsAsFactors = FALSE)
+source("../main_figures/parameters.R") ## pathFinalData are defined based on the user name
 
 sp="human"
 
-load(paste(pathFigures, "RData/", sp, ".cells.types.parallel.trends.RData", sep=""))
+load(paste(pathFigures, "RData/data.gene.annotations.RData", sep=""))
+load(paste(pathFigures, "RData/data.", sp, ".CM2019.SomaticOrgans.expdiv.RData", sep=""))
+load(paste(pathFigures, "RData/data.", sp, ".regland.conservation.RData", sep=""))
 
-cells <- c("Bcell", "ESC", "adipo")
-dataset.colors=c("firebrick1", "forestgreen", "navy")
-names(dataset.colors) = cells
+if (sp == "human"){sp_name="Human"}else{sp_name="Mouse"}
 
-pdf(file=paste(pathFigures, "/ExtendedFigure5.pdf", sep=""), width=7, height=6)
+bord.measure = c("#AF46B4", "#4BB446")
+names(bord.measure) = c("Spearman's rho", "1-Euclidean distance")
+col.measure = c("#AF46B41A", "#4BB4461A")
+names(col.measure) = c("Spearman's rho", "1-Euclidean distance")
+nb = 1
 
-m=matrix(rep(NA, 2*13), nrow=2)
-m[1,]=c(rep(1,4),  rep(2,3),  rep(3,3), rep(4,3))
-m[2,]=c(rep(5,4), rep(6,3),  rep(7,3), rep(8,3))
-layout(m)
+clearboxplot <- function(measure, divergences, plotlabel){
+  
+  if (measure == "classTau"){
+    labels=c("broad", "", "", "narrow")
+    xlab="expression breadth"
+  } else{
+    labels=c("low", "", "", "high")
+    xlab="mean expression level"
+  }
+  
+  xlim=c(0.5, length(levels(expdiv[[measure]]))+0.5)
+  
+  xpos=seq(1, length(levels(expdiv[[measure]])), 1)
+  names(xpos) = levels(expdiv[[measure]])
+  smallx=c(-0.15, 0.15)
+  names(smallx)=c("Spearman's rho", "1-Euclidean distance")
+ 
+  for (divergence in divergences){
+    if (any(grepl("Spearman", divergence))){
+      name="Spearman's rho"
+      ylim = c(-0.5, 1)
+    } else{
+      name="1-Euclidean distance"
+      ylim = c(0.55, 1)
+    }
+    
+    if (name=="Spearman's rho"){
+      par(mar=c(1.5, 3.75, 2.6, 1.1))
+    } else{
+      par(mar=c(4, 3.75, 0.6, 1.1))
+    }
+
+    plot(1, type="n", xlab="", ylab="", axes=F, xlim=xlim, ylim=ylim, xaxs="i", yaxs="i")
+
+    for(class in levels(expdiv[[measure]])){
+      x=xpos[class]+smallx[name]
+    
+      boxplot(expdiv[which(expdiv[[measure]] == class), divergence], at=x, border=bord.measure[name], col=col.measure[name],
+             boxwex=0.5, axes=F, add=T, notch=T, outline=F)
+    }
+    
+    abline(v=xpos[1:length(levels(expdiv[[measure]]))-1]+0.5, lty=3, col="gray40")
+    axis(side=2, mgp=c(3, 0.75, 0), cex.axis=1.1)
+    mtext(name, side=2, line=2.5, cex=CEXLAB)
+    
+    if (name=="Spearman's rho"){
+      mtext(plotlabel, side=3, line=1, at=-0.1, font=2, cex=1.2)
+    }else{
+      axis(side=1, at=xpos, mgp=c(3, 0.5, 0), labels=rep("", length(levels(expdiv[[measure]]))), cex.axis=0.8)
+      mtext(labels, at=xpos, side=1, line=1, cex=CEXLAB)
+      
+      mtext(xlab, side=1, line=2.5, cex=CEXLAB)
+    }
+  }
+  
+}
 
 ######################################################################################################################
-####################################### Parallel trends among cell types #############################################
-par(mai = c(0.7, 0.1, 0.5, 0.1)) # bottom, left, top, right
-cex.label=1
 
-##  Correlation of expression levels
+pdf(file=paste(pathFigures, "ExtendedFigure4.pdf", sep=""), width = 6.85)
 
-dotchart(correl_expression[rev(cells), "Spearman"], col=dataset.colors[rev(cells)], labels=c("pre-\nadipocytes", "ESC", "B cells"), pch=20, pt.cex=1.1,  xlim=c(min(correl_expression)-0.02, max(correl_expression)+0.02), cex.lab=1.25)
-mtext("expression correlation\n(Spearman's rho)", side=1, line=3.5, cex=0.7)
+m=matrix(rep(NA, 10*2), nrow=10)
+for(i in 1:3){
+  m[i,]=c(1,3)
+}
 
-## plot label
-mtext("a", side=3, at=0.71, font=2, cex=cex.label, line=1.5)
+for(i in 4:6){
+  m[i,]=c(2,4)
+}
+for(i in 7:10){
+  m[i,]=c(5,6)
+}
 
-#  Conserved Expression
-dotchart(conserv_expression[rev(cells),"Mean"], col=dataset.colors[rev(cells)], pch=20, labels="", pt.cex=1.1,
-         xlim=c(min(conserv_expression[,"Conf_low"])-0.01, max(conserv_expression[,"Conf_high"])+0.01))
-segments(x0=conserv_expression[rev(cells),"Conf_low"], x1=conserv_expression[rev(cells),"Conf_high"], y0=1:3, y1=1:3, col=dataset.colors[rev(cells)], lwd=1.1)
-## segments(x0=conserv_expression[rev(cells),"Conf_low"], x1=conserv_expression[rev(cells),"Conf_low"], y0=(1:3)-0.02, y1=(1:3)+0.02, col=dataset.colors[rev(cells)], lwd=1)
-## segments(x0=conserv_expression[rev(cells),"Conf_high"], x1=conserv_expression[rev(cells),"Conf_high"], y0=(1:3)-0.02, y1=(1:3)+0.02, col=dataset.colors[rev(cells)], lwd=1)
+layout(m)
 
-mtext("expression conservation\n(corrected)", side=1, line=3.5, cex=0.7)
+CEXLAB = 0.9
+CEXstats = 0.8
+expdiv$EuclideanSimilarity <- 1-expdiv$EuclideanDistance
 
-## plot label
-mtext("b", side=3, at=-0.11, font=2, cex=cex.label, line=1.5)
+################ Cofounding factors ################ 
+# Gene expression level
+expdiv$classRPKM <- cut2(expdiv$Human_MeanRPKM, g=4, include.lowest=T) 
+clearboxplot("classRPKM", c("CorrelationSpearman","EuclideanSimilarity"), "a")
 
-#  Correlattion of complexity zscore 
-dotchart(correl_complexity[rev(cells),"Spearman"], col=dataset.colors[rev(cells)], labels='', pch=20, pt.cex=1.1, 
-         xlim=c(min(correl_complexity[,"Spearman"])-0.01, max(correl_complexity[,"Spearman"])+0.01))
-mtext("conservation of regulatory complexity\n(Spearman's rho)", side=1, line=3.5, cex=0.7)
+# Specificity
+clearboxplot("classTau", c("CorrelationSpearman","EuclideanSimilarity"), "b")
 
-## plot label
-mtext("c", side=3, at=0.215, font=2, cex=cex.label, line=1.5)
+################ Correlation between Spearman and Euclidean  ################ 
+par(mar=c(4.5, 5, 3, 5)) # bottom, left, top, right
 
-# #  dN / dS
-dotchart(gene_dnds[rev(cells),"Mean"], col=dataset.colors[rev(cells)], labels='', pch=20, pt.cex=1.1,
-         xlim=c(min(gene_dnds[,"Conf_low"])-0.02, max(gene_dnds[,"Conf_high"])+0.02))
-segments(x0=gene_dnds[rev(cells),"Conf_low"], x1=gene_dnds[rev(cells),"Conf_high"], y0=1:3, y1=1:3, col=dataset.colors[rev(cells)], lwd=1.1)
-## segments(x0=gene_dnds[rev(cells),"Conf_low"], x1=gene_dnds[rev(cells),"Conf_low"], y0=(1:3)-0.02, y1=(1:3)+0.02, col=dataset.colors[rev(cells)], lwd=1)
-## segments(x0=gene_dnds[rev(cells),"Conf_high"], x1=gene_dnds[rev(cells),"Conf_high"], y0=(1:3)-0.02, y1=(1:3)+0.02, col=dataset.colors[rev(cells)], lwd=1)
+smoothScatter(expdiv$EuclideanSimilarity, expdiv$CorrelationSpearman, xlab="", ylab="", cex.axis=1.1)
 
-mtext("1- dN/dS \nof top expressed genes", side=1, line=3.5, cex=0.7)
+R=cor(expdiv$EuclideanSimilarity, expdiv$CorrelationSpearman,method="pearson")
+rho=cor(expdiv$EuclideanSimilarity, expdiv$CorrelationSpearman, method="spearman")
+x=expdiv$EuclideanSimilarity
+y=expdiv$CorrelationSpearman
+abline(lm(y~x), col="red")
 
-## plot label
-mtext("d", side=3, at=0.855, font=2, cex=cex.label, line=1.5)
+mtext(paste("Pearson's R = ", round(R, digits=2), ", rho = ",round(rho, digits=2),sep=""),
+      side=3, line=0.5, cex=CEXstats)
 
-#  Enhancer Alignment score
-dotchart(enh_evol[rev(cells),"Mean"], col=dataset.colors[rev(cells)], pch=20, labels=c("pre-\nadipocytes", "ESC", "B cells"), pt.cex=1.1,
-         xlim=c(min(enh_evol[,"Conf_low"])-0.02, max(enh_evol[,"Conf_high"])+0.02))
-segments(x0=enh_evol[rev(cells),"Conf_low"], x1=enh_evol[rev(cells),"Conf_high"], y0=1:3, y1=1:3, col=dataset.colors[rev(cells)])
-## segments(x0=enh_evol[rev(cells),"Conf_low"], x1=enh_evol[rev(cells),"Conf_low"], y0=(1:3)-0.02, y1=(1:3)+0.02, col=dataset.colors[rev(cells)], lwd=1)
-## segments(x0=enh_evol[rev(cells),"Conf_high"], x1=enh_evol[rev(cells),"Conf_high"], y0=(1:3)-0.02, y1=(1:3)+0.02, col=dataset.colors[rev(cells)], lwd=1)
+mtext("1-Euclidean distance", side=1, line=2.5, cex=CEXLAB)
+mtext("Spearman's rho", side=2, line=2.5, cex=CEXLAB)
+mtext("c", side=3, line=2, at=-0.01, font=2, cex=1.2)
 
-mtext("alignment score\n of contacted enhancers", side=1, line=3.5, cex=0.7)
+### Correlation between measures
+smoothScatter(expdiv$CorrectedEuclideanSimilarity, expdiv$CorrectedSpearman, xlab="", ylab="", cex.axis=1.1)
+R=cor(expdiv$CorrectedEuclideanSimilarity, expdiv$CorrectedSpearman,method="pearson")
+rho=cor(expdiv$CorrectedEuclideanSimilarity, expdiv$CorrectedSpearman, method="spearman")
+x=expdiv$CorrectedEuclideanSimilarity
+y=expdiv$CorrectedSpearman
+abline(lm(y~x), col="red")
 
-## plot label
-mtext("e", side=3, at=0.4725, font=2, cex=cex.label, line=1.5)
+mtext(paste("Pearson's R = ", round(R, digits=2), ", rho = ",round(rho, digits=2),sep=""),
+      side=3, line=0.5, cex=CEXstats)
 
-# Conserved sequence
-dotchart(seq_conserv[rev(cells),"Mean"], col=dataset.colors[rev(cells)], pch=20, labels='', pt.cex=1.1,
-         xlim=c(min(seq_conserv[,"Conf_low"])-0.02, max(seq_conserv[,"Conf_high"])+0.02))
-segments(x0=seq_conserv[rev(cells),"Conf_low"], x1=seq_conserv[rev(cells),"Conf_high"], y0=1:3, y1=1:3, col=dataset.colors[rev(cells)])
-## segments(x0=seq_conserv[rev(cells),"Conf_low"], x1=seq_conserv[rev(cells),"Conf_low"], y0=(1:3)-0.02, y1=(1:3)+0.02, col=dataset.colors[rev(cells)], lwd=1)
-## segments(x0=seq_conserv[rev(cells),"Conf_high"], x1=seq_conserv[rev(cells),"Conf_high"], y0=(1:3)-0.02, y1=(1:3)+0.02, col=dataset.colors[rev(cells)], lwd=1)
+mtext("1-Euclidean distance (corrected)", side=1, line=2.5, cex=CEXLAB)
+mtext("Spearman's rho (corrected)", side=2, line=2.5, cex=CEXLAB)
+mtext("d", side=3, line=2, at=-0.8, font=2, cex=1.2)
 
-mtext("proportion of conserved\n enhancers by gene", side=1, line=3.5, cex=0.7)
-
-## plot label
-mtext("f", side=3, at=0.53, font=2, cex=cex.label, line=1.5)
-
-#  Conserved synteny
-dotchart(synteny_conserv[rev(cells),"Mean"], col=dataset.colors[rev(cells)], pch=20, labels='', pt.cex=1.1,
-         xlim=c(min(synteny_conserv[,"Conf_low"])-0.04, max(synteny_conserv[,"Conf_high"])+0.04))
-segments(x0=synteny_conserv[rev(cells),"Conf_low"], x1=synteny_conserv[rev(cells),"Conf_high"], y0=1:3, y1=1:3, col=dataset.colors[rev(cells)], lwd=1)
-## segments(x0=synteny_conserv[rev(cells),"Conf_low"], x1=synteny_conserv[rev(cells),"Conf_low"], y0=(1:3)-0.02, y1=(1:3)+0.02, col=dataset.colors[rev(cells)], lwd=1)
-## segments(x0=synteny_conserv[rev(cells),"Conf_high"], x1=synteny_conserv[rev(cells),"Conf_high"], y0=(1:3)-0.02, y1=(1:3)+0.02, col=dataset.colors[rev(cells)], lwd=1)
-
-mtext("proportion of enhancers\nmaintained in synteny", side=1, line=3.5, cex=0.7)
-## plot label
-mtext("g", side=3, at=0.85, font=2, cex=cex.label, line=1.5)
-
-#  Conserved contacts
-dotchart(contact_conserv[rev(cells),"Mean"], col=dataset.colors[rev(cells)], pch=20, labels='', pt.cex=1.1,
-         xlim=c(min(contact_conserv[,"Conf_low"])-0.04, max(contact_conserv[,"Conf_high"])+0.04))
-segments(x0=contact_conserv[rev(cells),"Conf_low"], x1=contact_conserv[rev(cells),"Conf_high"], y0=1:3, y1=1:3, col=dataset.colors[rev(cells)], lwd=1)
-## segments(x0=contact_conserv[rev(cells),"Conf_low"], x1=contact_conserv[rev(cells),"Conf_low"], y0=(1:3)-0.02, y1=(1:3)+0.02, col=dataset.colors[rev(cells)], lwd=1)
-## segments(x0=contact_conserv[rev(cells),"Conf_high"], x1=contact_conserv[rev(cells),"Conf_high"], y0=(1:3)-0.02, y1=(1:3)+0.02, col=dataset.colors[rev(cells)], lwd=1)
-
-mtext("percentage of enhancers\nmaintained in contact", side=1, line=3.5, cex=0.7)
-
-## plot label
-mtext("h", side=3, at=0.12, font=2, cex=cex.label, line=1.5)
-
-##############################################################################################################
+##############################################################################################
 
 dev.off()
 
-##############################################################################################################
+##############################################################################################
+
