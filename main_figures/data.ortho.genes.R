bdcafefb1e30efd29f4028e3dbbd519681001afd
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2021-01-31 18:31:50 +0100

    added dN/dS filter

--- main_figures/data.ortho.genes.R
+++ main_figures/data.ortho.genes.R
@@ -5,17 +5,23 @@ source("parameters.R")
 pathEvolution=paste(pathFinalData, "SupplementaryDataset7/", sep="")
 
 ###########################################################################
+
 ## only for human and mouse
 
-all.ortho=read.table(paste(pathEvolution, "human/gene_orthology/human2mouse_orthologue.txt", sep=""), h=T, stringsAsFactors=F, sep="\t", quote="\"")
+all.ortho=read.table(paste(pathEvolution, "human/gene_orthology/human2mouse_orthologue_dNdS.txt", sep=""), h=T, stringsAsFactors=F, sep="\t", quote="\"")
+colnames(all.ortho)=c("IDHuman", "IDMouse", "HomologyType", "dN", "dS")
+
+all.ortho$dN=as.numeric(all.ortho$dN)
+all.ortho$dS=as.numeric(all.ortho$dS)
+all.ortho$dNdS=all.ortho$dN/all.ortho$dS
 
 ###########################################################################
 
-ortho=all.ortho[which(all.ortho$Mouse.homology.type=="ortholog_one2one"), c("Gene.stable.ID", "Mouse.gene.stable.ID")]
+ortho=all.ortho[which(all.ortho$HomologyType=="ortholog_one2one" & all.ortho$dNdS<1), c("IDHuman", "IDMouse")]
 colnames(ortho)=c("human", "mouse")
 
 ###########################################################################
 
-save(list=c("ortho"), file=paste(pathFigures, "RData/data.ortho.genes.RData", sep=""))
+save(ortho, file=paste(pathFigures, "RData/data.ortho.genes.RData", sep=""))
 
 ###########################################################################
