54f0845ea4fe1160a731ed180ab0a86622632cc6
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2021-02-01 09:36:12 +0100

    renamed extended figures

--- sup_figures/ExtendedFigure5.R
+++ sup_figures/ExtendedFigure5.R
@@ -1,13 +1,13 @@
-######################################################################################################################
-library(Hmisc)
-options(stringsAsFactors = FALSE)
+###########################################################################################################
+
 source("../main_figures/parameters.R") ## pathFinalData are defined based on the user name
 
 sp="human"
+sp_name="Human"
 
 load(paste(pathFigures, "RData/data.gene.annotations.RData", sep=""))
-load(paste(pathFigures, "RData/data.", sp, ".CM2019.SomaticOrgans.expdiv.RData", sep=""))
-load(paste(pathFigures, "RData/data.", sp, ".regland.conservation.RData", sep=""))
+load(paste(pathFigures, "RData/data.common.cells.expdiv.RData", sep=""))
+load(paste(pathFigures, "RData/data.", sp, ".common.cells.regland.conservation.RData", sep=""))
 
 if (sp == "human"){
   sp_name="Human"
@@ -15,135 +15,167 @@ if (sp == "human"){
   sp_name="Mouse"
 }
 
-bord.measure = c("#AF46B4", "#4BB446")
-names(bord.measure) = c("Spearman's rho", "1-Euclidean distance")
-col.measure = c("#AF46B41A", "#4BB4461A")
-names(col.measure) = c("Spearman's rho", "1-Euclidean distance")
-nb = 1
+cells <- c("Bcell", "ESC", "adipo")
+col.cells = c("navy", "forestgreen", "darkorange")
+names(col.cells) = cells
+enh = "ENCODE"
 
-clearboxplot <- function(measure, divergences, plotlabel){
-  
-  if (measure == "classTau"){
-    labels=c("broad", "", "", "narrow")
-    xlab="expression breadth"
-  } else{
-    labels=c("low", "", "", "high")
-    xlab="mean expression level"
+#############################################################################################################
+######################## Gene expression level in common cell types #########################################
+
+xpos=seq(1, 5, 1)
+smallx=c(-0.15, 0, 0.15)
+names(smallx)=cells
+xlim=c(0.5, 5.5)
+
+CellTypesPlot <- function(var, plot.nb){
+  if (var == "ExpressionConservation"){
+    ylim=c(0.2, 0.6)
+  }
+  else if (var == paste0(sp, "_MeanRPKM")){
+    ylim=c(0, 22)
+  }
+  else if (var == "ResidualExpressionConservation"){
+    ylim=c(-0.11, 0.03)
   }
   
-  xlim=c(0.5, length(levels(expdiv[[measure]]))+0.5)
-  
-  xpos=seq(1, length(levels(expdiv[[measure]])), 1)
-  names(xpos) = levels(expdiv[[measure]])
-  smallx=c(-0.15, 0.15)
-  names(smallx)=c("Spearman's rho", "1-Euclidean distance")
- 
-  for (divergence in divergences){
-    if (any(grepl("Spearman", divergence))){
-      name="Spearman's rho"
-      ylim = c(-0.5, 1)
-    } else{
-      name="1-Euclidean distance"
-      ylim = c(0.55, 1)
-    }
-    
-    if (name=="Spearman's rho"){
-      par(mar=c(1.5, 3.75, 2.6, 1.1))
-    } else{
-      par(mar=c(4, 3.75, 0.6, 1.1))
-    }
+  plot(1, type="n", xlab="", ylab="", axes=F, xlim=xlim, ylim=ylim, xaxs="i", yaxs="i")
 
-    plot(1, type="n", xlab="", ylab="", axes=F, xlim=xlim, ylim=ylim, xaxs="i", yaxs="i")
-
-    for(class in levels(expdiv[[measure]])){
-      x=xpos[class]+smallx[name]
-    
-      boxplot(expdiv[which(expdiv[[measure]] == class), divergence], at=x, border=bord.measure[name], col=col.measure[name],
-             boxwex=0.5, axes=F, add=T, notch=T, outline=F)
-    }
-    
-    abline(v=xpos[1:length(levels(expdiv[[measure]]))-1]+0.5, lty=3, col="gray40")
-    axis(side=2, mgp=c(3, 0.75, 0), cex.axis=1.1)
-    mtext(name, side=2, line=2.5, cex=CEXLAB)
-    
-    if (name=="Spearman's rho"){
-      mtext(plotlabel, side=3, line=1, at=-0.1, font=2, cex=1.2)
-    }else{
-      axis(side=1, at=xpos, mgp=c(3, 0.5, 0), labels=rep("", length(levels(expdiv[[measure]]))), cex.axis=0.8)
-      mtext(labels, at=xpos, side=1, line=1, cex=CEXLAB)
+  for (cell in cells){
+    regland = genes.conservation.cells[[enh]][[cell]][["obs"]][["all"]]
+      genes = intersect(rownames(regland), rownames(expdiv_cells))
+      regland = regland[genes,]
       
-      mtext(xlab, side=1, line=2.5, cex=CEXLAB)
+      for(class in levels(regland$class_nb_contact)){
+        this.genes=rownames(regland[which(regland$class_nb_contact == class),])
+        
+        xpos=seq(1, length(levels(regland$class_nb_contact)), 1)
+        names(xpos) = levels(regland$class_nb_contact)
+        x=xpos[class]+smallx[cell]
+        
+        b=boxplot(expdiv_cells[this.genes, paste(cell, var, sep="_")], plot=FALSE)
+        med=median(expdiv_cells[this.genes, paste(cell, var, sep="_")])
+        ci=as.numeric(b$conf)
+        
+        points(x, med, pch=20, col=col.cells[cell], cex=1.1)
+        segments(x, ci[1], x, ci[2],  col=col.cells[cell])
+        
+      }
+    }
+  
+  abline(v=xpos[1:4]+0.5, lty=3, col="gray40") # verticale lines between quantile
+  
+  axis(side=2, mgp=c(3, 0.75, 0), cex.axis=1) # Yaxis labels in first column
+  mtext(names_MeasuresCellTypes[plot.nb], side=2, line=2.5, cex=0.75)
+  
+  ## plot labels
+  mtext(letters[plot.nb], side=3, line=1, at=-0.8, font=2, cex=1.1)
+  
+  axis(side=1, cex.axis=1.1, mgp=c(3, 0.75, 0), at=1:5, labels=rep("",5)) # X axis labels in last row
+  mtext(c("low", "medium", "high"), at=xpos[c(1,3,5)], side=1, line=0.75, cex=0.75)
+  mtext("number of contacts", side=1, line=2, cex=0.75)
+  
+  if (plot.nb == 1){
+    legend("topleft", legend=c("pre-adipocytes", "ESC", "B cells"), pch=20, col=rev(col.cells), cex=1.1, bty="o", 
+           box.col="white", bg="white",  inset=c(0.01, -0.1), xpd=NA)
+  }
+}
+
+
+################################################################################################################################
+################### Gene expression evolution to Regulatory Landscape Evolution ################################################
+n = 1
+
+plot_cell <- function(class_conserv, Measure, distances, xlab, xnames){
+  smallx=c(-0.15, 0, 0.15)
+  names(smallx)=cells
+  
+  if (Measure == "corrected"){
+    DivergenceMeasure = "ResidualExpressionConservation"
+    ylab="expression conservation\n(corrected)"
+    YLIM=c(-0.15, 0.11)
+  } else{
+    DivergenceMeasure = "ExpressionConservation"
+    ylab="expression conservation"
+    YLIM=c(0.1,0.75)
+  } 
+  
+  if (class_conserv == "class_cons_synt"){
+    xmax=3
+  }else{
+    xmax=5
+  }
+  
+  xlim=c(0.5, xmax+0.5)
+  
+  plot(1, type="n", xlab="", ylab="", axes=F, xlim=xlim, ylim=YLIM, xaxs="i", yaxs="i")
+  
+  for (cell in cells){
+    regland = genes.conservation.cells[[enh]][[cell]][["obs"]][["all"]]
+    genes = intersect(rownames(regland), rownames(expdiv_cells))
+    regland = regland[genes,]
+    
+    for (class in levels(regland[[class_conserv]])){
+      this.genes=rownames(regland[which(regland[[class_conserv]] == class),])
+      
+      b=boxplot(expdiv_cells[this.genes, paste(cell, "_", DivergenceMeasure, sep="")], plot=F)
+      med=median(expdiv_cells[this.genes, paste(cell, "_", DivergenceMeasure, sep="")])
+      ci=as.numeric(b$conf)
+      
+      xpos=seq(1,  length(levels(regland[[class_conserv]])), 1)
+      names(xpos) = levels(regland[[class_conserv]])
+      
+      x=xpos[class]+smallx[cell]
+      
+      points(x, med, pch=20, col=col.cells[cell], cex=1.1)
+      segments(x, ci[1], x, ci[2], col=col.cells[cell])
     }
   }
   
+  abline(v=xpos[1:xmax-1]+0.5, lty=3, col="gray40")
+  axis(side=1, at=xpos, mgp=c(3, 0.5, 0), labels=rep("", length(levels(regland[[class_conserv]]))), cex.axis=0.8)
+
+  if(class_conserv=="class_align_score"){
+    mtext(c("low", "medium", "high"), at=xpos[c(1,3,5)], side=1, line=0.75, cex=0.75)
+  } else{
+    mtext(xnames, at=xpos, side=1, line=0.75, cex=0.75)
+  }
+  mtext(xlab, side=1, line=2.25, cex=0.75)
+  
+  axis(side=2, mgp=c(3, 0.75, 0), cex.axis=1)
+  mtext(ylab, side=2, line=2.5, cex=0.75)
+  
 }
 
-######################################################################################################################
+################################################################################################################################
+################################################### Output #####################################################################
+# Relation with complexity
+MeasuresCellTypes <- c(paste0(sp, "_MeanRPKM"), "ExpressionConservation", "ResidualExpressionConservation") 
+names_MeasuresCellTypes <- c("expression level (TPM)", "expression conservation", "expression conservation\n(corrected)")
 
-pdf(file=paste(pathFigures, "ExtendedFigure5.pdf", sep=""), width = 6.85)
+pdf(paste(pathFigures, "/ExtendedFigure4.pdf", sep=""), width=6.85, height=5.5)
 
-m=matrix(rep(NA, 10*2), nrow=10)
-for(i in 1:3){
-  m[i,]=c(1,3)
+par(mfrow=c(2,3))
+par(mar = c(3.5, 4.65, 2.5, 1.1)) # bottom, left, top, right
+
+for (measure in 1:length(MeasuresCellTypes)){
+  CellTypesPlot(MeasuresCellTypes[measure], measure)
 }
 
-for(i in 4:6){
-  m[i,]=c(2,4)
-}
-for(i in 7:10){
-  m[i,]=c(5,6)
-}
+# Relation with Regulatory Landscape Evolution
+distances =  "all"  # c("25kb - 100kb", "100kb - 500kb", "500kb - 2Mb", "all")
 
-layout(m)
+for (measure in c("corrected")){
+  plot_cell("class_align_score", measure, distances, "enhancer sequence conservation", 1:5)
+  mtext("d", side=3, at=-0.65, font=2, cex=1.1, line=1)
 
-CEXLAB = 0.9
-CEXstats = 0.8
-expdiv$EuclideanSimilarity <- 1-expdiv$EuclideanDistance
-
-################ Cofounding factors ################ 
-# Gene expression level
-expdiv$classRPKM <- cut2(expdiv$Human_MeanRPKM, g=4, include.lowest=T) 
-clearboxplot("classRPKM", c("CorrelationSpearman","EuclideanSimilarity"), "a")
-
-# Specificity
-clearboxplot("classTau", c("CorrelationSpearman","EuclideanSimilarity"), "b")
-
-################ Correlation between Spearman and Euclidean  ################ 
-par(mar=c(4.5, 5, 3, 5)) # bottom, left, top, right
-
-smoothScatter(expdiv$EuclideanSimilarity, expdiv$CorrelationSpearman, xlab="", ylab="", cex.axis=1.1)
-
-R=cor(expdiv$EuclideanSimilarity, expdiv$CorrelationSpearman,method="pearson")
-rho=cor(expdiv$EuclideanSimilarity, expdiv$CorrelationSpearman, method="spearman")
-x=expdiv$EuclideanSimilarity
-y=expdiv$CorrelationSpearman
-abline(lm(y~x), col="red")
-
-mtext(paste("Pearson's R = ", round(R, digits=2), ", rho = ",round(rho, digits=2),sep=""),
-      side=3, line=0.5, cex=CEXstats)
-
-mtext("1-Euclidean distance", side=1, line=2.5, cex=CEXLAB)
-mtext("Spearman's rho", side=2, line=2.5, cex=CEXLAB)
-mtext("c", side=3, line=2, at=-0.01, font=2, cex=1.2)
-
-### Correlation between measures
-smoothScatter(expdiv$CorrectedEuclideanSimilarity, expdiv$CorrectedSpearman, xlab="", ylab="", cex.axis=1.1)
-R=cor(expdiv$CorrectedEuclideanSimilarity, expdiv$CorrectedSpearman,method="pearson")
-rho=cor(expdiv$CorrectedEuclideanSimilarity, expdiv$CorrectedSpearman, method="spearman")
-x=expdiv$CorrectedEuclideanSimilarity
-y=expdiv$CorrectedSpearman
-abline(lm(y~x), col="red")
-
-mtext(paste("Pearson's R = ", round(R, digits=2), ", rho = ",round(rho, digits=2),sep=""),
-      side=3, line=0.5, cex=CEXstats)
-
-mtext("1-Euclidean distance (corrected)", side=1, line=2.5, cex=CEXLAB)
-mtext("Spearman's rho (corrected)", side=2, line=2.5, cex=CEXLAB)
-mtext("d", side=3, line=2, at=-0.8, font=2, cex=1.2)
-
-##############################################################################################
+  plot_cell("class_cons_synt",  measure, distances, "% conserved synteny", c("<75%", "75-99%", ">99%"))
+  mtext("e", side=3, at=-0.2, font=2, cex=1.1, line=1)
+  
+  plot_cell("class_cons_cont",  measure, distances, "% conserved contacts", c("<1%", "25%", "50%", "75", ">75%"))
+  mtext("f", side=3, at=-0.65, font=2, cex=1.1, line=1)
+} 
 
 dev.off()
 
-##############################################################################################
-
+################################################################################################################################
