a7eb4a627758315a75f449e1f6499f01c1a35df8
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2021-02-02 19:26:30 +0100

    corrected typo

--- supp_datasets/cleanup.synteny.conservation.data.R
+++ supp_datasets/cleanup.synteny.conservation.data.R
@@ -24,7 +24,7 @@ for(ref in c("human","mouse")){
         data=read.table(paste(pathEvolution, ref, "/synteny_conservation/",enh,"/",ref,"2",tg,"_",type,"_synteny.txt",sep=""), h=T, stringsAsFactors=F)
         data=data[,which(!(colnames(data)%in%exclude))]
         
-        system(paste("mv ", pathEvolution, ref, "/synteny_conservation/",enh,"/",ref,"2",tg,"_",type,"_synteny.txt ",pathEvolution, ref, "/synteny_conservation/",enh,"/backup_",ref,"2",tg,"_",data,"_synteny.txt ", sep=""))
+        system(paste("mv ", pathEvolution, ref, "/synteny_conservation/",enh,"/",ref,"2",tg,"_",type,"_synteny.txt ",pathEvolution, ref, "/synteny_conservation/",enh,"/backup_",ref,"2",tg,"_",type,"_synteny.txt ", sep=""))
         
         write.table(data, file=paste(pathEvolution, ref, "/synteny_conservation/",enh,"/",ref,"2",tg,"_",type,"_synteny.txt",sep=""), row.names=F, col.names=T, sep="\t", quote=F)
       }
