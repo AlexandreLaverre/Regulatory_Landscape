423b6038c56eaf4ee20479bed139ee1c86d67fda
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2021-02-02 19:21:51 +0100

    added progress report

--- supp_datasets/cleanup.contact.conservation.data.R
+++ supp_datasets/cleanup.contact.conservation.data.R
@@ -18,6 +18,8 @@ for(ref in c("human","mouse")){
   for(enh in enhancer.datasets[[ref]]){
      for(refdata in c("original", "simulated")){
        for(tgdata in c("original", "simulated")){
+
+         print(paste(ref, tg, enh, refdata, tgdata))
          
          data=read.table(paste(pathEvolution, ref, "/contact_conservation/", enh,"/",ref,"_",refdata,"2",tg,"_",tgdata,".txt",sep=""), h=T, stringsAsFactors=F)
 
@@ -25,7 +27,7 @@ for(ref in c("human","mouse")){
 
          system(paste("mv ",pathEvolution, ref, "/contact_conservation/", enh,"/",ref,"_",refdata,"2",tg,"_",tgdata,".txt ",pathEvolution, ref, "/contact_conservation/", enh,"/backup_",ref,"_",refdata,"2",tg,"_",tgdata,".txt ",sep=""))
          
-         write.table(data, file=paste(pathEvolution, ref, "contact_conservation/", enh,"/",ref,"_",refdata,"2",tg,"_",tgdata,".txt",sep=""), row.names=F, col.names=T, sep="\t", quote=F)
+         write.table(data, file=paste(pathEvolution, ref, "/contact_conservation/", enh,"/",ref,"_",refdata,"2",tg,"_",tgdata,".txt",sep=""), row.names=F, col.names=T, sep="\t", quote=F)
        }
      }
    }
--- supp_datasets/cleanup.synteny.conservation.data.R
+++ supp_datasets/cleanup.synteny.conservation.data.R
@@ -18,13 +18,15 @@ allspecies=c("human", "macaque", "mouse", "rat", "rabbit", "cow", "dog", "elepha
 for(ref in c("human","mouse")){
   for(tg in setdiff(allspecies, ref)){
     for(enh in enhancer.datasets[[ref]]){
-      for(data in c("original", "simulated")){
-        data=read.table(paste(pathEvolution, ref, "/synteny_conservation/",enh,"/",ref,"2",tg,"_",data,"_synteny.txt",sep=""), h=T, stringsAsFactors=F)
+      for(type in c("original", "simulated")){
+        print(paste(ref, tg, enh, type))
+        
+        data=read.table(paste(pathEvolution, ref, "/synteny_conservation/",enh,"/",ref,"2",tg,"_",type,"_synteny.txt",sep=""), h=T, stringsAsFactors=F)
         data=data[,which(!(colnames(data)%in%exclude))]
         
-        system(paste("mv ", pathEvolution, ref, "/synteny_conservation/",enh,"/",ref,"2",tg,"_",data,"_synteny.txt ",pathEvolution, ref, "/synteny_conservation/",enh,"/backup_",ref,"2",tg,"_",data,"_synteny.txt ", sep=""))
+        system(paste("mv ", pathEvolution, ref, "/synteny_conservation/",enh,"/",ref,"2",tg,"_",type,"_synteny.txt ",pathEvolution, ref, "/synteny_conservation/",enh,"/backup_",ref,"2",tg,"_",data,"_synteny.txt ", sep=""))
         
-        write.table(data, file=paste(pathEvolution, ref, "/synteny_conservation/",enh,"/",ref,"2",tg,"_",data,"_synteny.txt",sep=""), row.names=F, col.names=T, sep="\t", quote=F)
+        write.table(data, file=paste(pathEvolution, ref, "/synteny_conservation/",enh,"/",ref,"2",tg,"_",type,"_synteny.txt",sep=""), row.names=F, col.names=T, sep="\t", quote=F)
       }
     }
   }
