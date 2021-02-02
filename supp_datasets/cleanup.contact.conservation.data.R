3e2dde1c3ddae6220cc1bd51caadd2414cac8d36
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2021-02-02 18:48:22 +0100

    removed unused columns in contact conservation

---
+++ supp_datasets/cleanup.contact.conservation.data.R
@@ -0,0 +1,34 @@
+#####################################################################################
+
+pathEvolution="/beegfs/data/necsulea/RegulatoryLandscapesManuscript/SupplementaryDataset7/"
+
+#####################################################################################
+
+enhancer.datasets=list()
+enhancer.datasets[["human"]]=c("ENCODE", "FANTOM5", "FOCS_GRO_seq", "RoadmapEpigenomics")
+enhancer.datasets[["mouse"]]=c("ENCODE", "FANTOM5")
+
+exclude=c("length_enh", "repeat_part", "GC_rate", "BLAT_match", "align_score")
+
+#####################################################################################
+
+for(ref in c("human","mouse")){
+  tg=setdiff(c("human", "mouse"), ref)
+
+  for(enh in enhancer.datasets[[ref]]){
+     for(refdata in c("original", "simulated")){
+       for(tgdata in c("original", "simulated")){
+         
+         data=read.table(paste(pathEvolution, ref, "/contact_conservation/", enh,"/",ref,"_",refdata,"2",tg,"_",tgdata,".txt",sep=""), h=T, stringsAsFactors=F)
+
+         data=data[,which(!(colnames(data)%in%exclude))]
+
+         system(paste("mv ",pathEvolution, ref, "/contact_conservation/", enh,"/",ref,"_",refdata,"2",tg,"_",tgdata,".txt ",pathEvolution, ref, "/contact_conservation/", enh,"/backup_",ref,"_",refdata,"2",tg,"_",tgdata,".txt ",sep=""))
+         
+         write.table(data, file=paste(pathEvolution, ref, "contact_conservation/", enh,"/",ref,"_",refdata,"2",tg,"_",tgdata,".txt",sep=""), row.names=F, col.names=T, sep="\t", quote=F)
+       }
+     }
+   }
+}
+
+#####################################################################################
