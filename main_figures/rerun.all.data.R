e0b1c57d02d62e866469a694a89d3c71b5c1cff0
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2021-02-04 15:19:37 +0100

    changed rerun order

--- main_figures/rerun.all.data.R
+++ main_figures/rerun.all.data.R
@@ -2,7 +2,7 @@
 
 ## all scripts that generate .RData objects for the figures
 ## we need a certain order
-data.scripts=c("data.ortho.genes.R", "data.sample.info.R", "data.aberrant.fragments.R", "data.fragment.statistics.R", "data.enhancer.statistics.R", "data.bait.annotation.R",  "data.fragment.contacts.R", "data.gene.enhancer.contacts.R",  "data.Shh.figure.R",  "data.sample.clustering.R", "data.enhancer.coverage.R", "data.sequence.conservation.fragments.R", "data.sequence.conservation.enhancers.R","data.sequence.conservation.stats.R", "data.synteny.conservation.R", "data.gene.annotations.R", "data.gene.expression.R", "data.promoter.enhancer.correlation.R",  "data.contact.conservation.enhancers.R", "data.contact.conservation.stats.R", "data.AFC.R", "data.CM2019.SomaticOrgans.expression.divergence.R",  "data.regland.conservation.R")
+data.scripts=c("data.gene.annotations.R", "data.ortho.genes.R", "data.sample.info.R", "data.aberrant.fragments.R", "data.fragment.statistics.R", "data.enhancer.statistics.R", "data.bait.annotation.R",  "data.fragment.contacts.R", "data.gene.enhancer.contacts.R",  "data.Shh.figure.R",  "data.sample.clustering.R", "data.enhancer.coverage.R", "data.sequence.conservation.fragments.R", "data.sequence.conservation.enhancers.R","data.sequence.conservation.stats.R", "data.synteny.conservation.R", "data.gene.expression.R", "data.promoter.enhancer.correlation.R",  "data.contact.conservation.enhancers.R", "data.contact.conservation.stats.R", "data.AFC.R", "data.CM2019.SomaticOrgans.expression.divergence.R",  "data.regland.conservation.R")
 
 ############################################################################
 
