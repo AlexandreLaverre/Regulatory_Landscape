5db5ce353df54de3b5936249be2d7425de9d7fbd
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2021-06-01 23:27:31 +0200

    split bootstrap from fig 9

--- sup_figures/rerun.all.figures.R
+++ sup_figures/rerun.all.figures.R
@@ -4,16 +4,12 @@
 all.files=system("ls", intern=T)
 fig.scripts=grep("Figure", all.files, value=T)
 
-fig.scripts.slow=c("SupplementaryFigure3_SupplementaryMaterialFigure22.R","SupplementaryMaterialFigure17.R", "SupplementaryMaterialFigure18.R", "SupplementaryMaterialFigure19.R","SupplementaryMaterialFigure20.R", "SupplementaryMaterialFigure32-33.R","SupplementaryMaterialFigure8.R")
-
-fig.scripts.fast=setdiff(fig.scripts, fig.scripts.slow)
-
 ############################################################################
 
-for(file in fig.scripts.fast){
+for(file in fig.scripts){
   ## cleanup
   objects=ls()
-  objects=setdiff(objects, c("fig.scripts.fast", "fig.scripts.slow", "file"))
+  objects=setdiff(objects, c("fig.scripts"))
   rm(list=objects)
 
   ## run the script
@@ -22,25 +18,3 @@ for(file in fig.scripts.fast){
 }
 
 ############################################################################
-
-print("Do you want to redo figures with BCa computations?")
-
-consent=scan(what=character(), nmax=1)
-
-if(consent=="yes"){
-  
-  for(file in fig.scripts.slow){
-    ## cleanup
-    objects=ls()
-    objects=setdiff(objects, c("fig.scripts.slow", "file"))
-    rm(list=objects)
-    
-    ## run the script
-    print(file)
-    source(file)
-  }
-}
-
-############################################################################
-
-
