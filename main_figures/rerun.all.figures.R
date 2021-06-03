8f972f98bdb2e0d893b8e9c3ee03b022a0b22216
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2021-06-03 20:38:26 +0200

    rerun all figures

--- main_figures/rerun.all.figures.R
+++ main_figures/rerun.all.figures.R
@@ -1,14 +1,14 @@
 
 ## all scripts that generate figures
 
-fig.scripts.fast=c( "Figure2.R", "Figure4.R", "Figure5.R", "Figure6.R")
+fig.scripts.=c("Figure1.R", "Figure3.R", "Figure2.R", "Figure4.R", "Figure5.R", "Figure6.R")
 
 ############################################################################
 
-for(file in fig.scripts.fast){
+for(file in fig.scripts){
   ## cleanup
   objects=ls()
-  objects=setdiff(objects, c("fig.scripts.fast", "file"))
+  objects=setdiff(objects, c("fig.scripts", "file"))
   rm(list=objects)
 
   ## run the script
@@ -17,26 +17,3 @@ for(file in fig.scripts.fast){
 }
 
 ############################################################################
-
-fig.scripts.slow=c("Figure1.R", "Figure3.R")
-
-print("Do you want to redo figures 1 and 3 (BCa computations)?")
-
-consent=scan(what=character(), nmax=1)
-
-if(consent=="yes"){
-
-  for(file in fig.scripts.slow){
-    ## cleanup
-    objects=ls()
-    objects=setdiff(objects, c("fig.scripts.slow", "file"))
-    rm(list=objects)
-    
-    ## run the script
-    print(file)
-    source(file)
-  }
-}
-
-############################################################################
-
