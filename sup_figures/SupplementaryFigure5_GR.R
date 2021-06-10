42dd5d14aea8614e5fd70d7286c0a19785c1ad67
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2021-06-10 07:52:21 +0200

    SHH exp fig

--- sup_figures/SupplementaryFigure5_GR.R
+++ sup_figures/SupplementaryFigure5_GR.R
@@ -28,10 +28,14 @@ if(prepare){
 
 ###########################################################################################
 
-pdf(paste(pathFigures, "GenomeResearch_Figures/Supplemental_Fig_S5.pdf", sep=""), width=6.85, height=3.5)
+pdf(paste(pathFigures, "GenomeResearch_Figures/Supplemental_Fig_S5.pdf", sep=""), width=6.85, height=2.5)
 
 par(mar=c(0.25, 1, 0.15, 0.25))
-plot(shh.img, axes=F, ylim=c(210, 20), xlim=c(0, 400))
+plot(shh.img, axes=F, ylim=c(300, 10), xlim=c(0, 1100))
+
+mtext("SHH", side=3, font=3, adj=1, line=-1, at=400, cex=0.7)
+mtext("expression pattern (HPA RNA-seq data)", side=3, font=1, adj=0, line=-1, at=410, cex=0.7)
+mtext("expression level (TPM)", side=2, line=0, cex=0.7)
 
 ###########################################################################################
 
