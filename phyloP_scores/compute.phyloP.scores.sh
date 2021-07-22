f1f14b8794d466c17fac504b63b0be34ffb77f6a
Alexandre Laverré <alaverre@pbil-deb.univ-lyon1.fr>
2021-07-22 17:48:23 +0200

    changes path

--- phyloP_scores/compute.phyloP.scores.sh
+++ phyloP_scores/compute.phyloP.scores.sh
@@ -3,15 +3,20 @@
 ################ Input parameters #####################
 export species=$1         # i.e : human or mouse
 export way=$2             # i.e : 30 60 or 100way
-export enhancer=$3        # i.e : FANTOM5 ENCODE RoadmapEpigenomics FOCS_GRO_seq contacted_sequence_original contacted_sequence_simulated
+export enhancer=$3        # i.e : FANTOM5 ENCODE RoadmapEpigenomics FOCS_GRO_seq restriction_fragments
 
 ################ Export paths #####################
 export path=/beegfs/data/alaverre/Regulatory_landscape/
 export pathAnouk=/beegfs/data/necsulea/RegulatoryLandscapesManuscript/
 
-export pathEnhancers=${pathAnouk}/SupplementaryDataset5/${species}/${enhancer}
-export pathphyloP=${path}/data/phyloP/${species}/${way}
 
+if [ "${enhancer}" = "restriction_fragments" ]; then
+export pathEnhancers=${pathAnouk}/SupplementaryDataset1/${species}
+else
+export pathEnhancers=${pathAnouk}/SupplementaryDataset4/${species}/${enhancer}
+fi
+
+export pathphyloP=${path}/data/phyloP/${species}/${way}
 export pathResults=${path}/result/phyloP/${species}/${enhancer}
 export pathScripts=${path}/scripts/phyloP_scores
 
@@ -27,6 +32,13 @@ if [ "${species}" = "mouse" ]; then
 elif [ "${species}" = "human" ]; then
   export chromosomes=({1..22} X Y)
   export genome=hg38
+fi
+
+
+if [ "${enhancer}" = "restriction_fragments" ]; then
+export CoordSuffix=frag_coords_${genome}.bed
+else
+export CoordSuffix=enhancer_coordinates.bed
 fi
 
 ################ Create dir output #####################
@@ -52,11 +64,10 @@ do
 	    echo "#PBS -o std_output_phyloP.txt" >> ${pathScripts}/log/phyloP_${way}_${species}_${enhancer}_chr${chr}_${suffixExons}
 	    echo "#PBS -e std_error_phyloP.txt" >> ${pathScripts}/log/phyloP_${way}_${species}_${enhancer}_chr${chr}_${suffixExons}
 	    
-	    echo "perl ${pathScripts}/compute.phyloP.scores.pl --pathCoords=${pathEnhancers}/enhancer_coordinates.bed   --pathMaskExonBlocks=${pathExons} --pathPhastCons=${pathPhyloP_scores} --chr=chr${chr} --pathOutput=${pathResults}/phyloP_${way}_chr${chr}_${suffixExons}.txt" >> ${pathScripts}/log/phyloP_${way}_${species}_${enhancer}_chr${chr}_${suffixExons}
-	    #--pathCoords=${pathEnhancers}/statistics_${enhancer}.txt
+	    echo "perl ${pathScripts}/compute.phyloP.scores.pl --pathCoords=${pathEnhancers}/${CoordSuffix}   --pathMaskExonBlocks=${pathExons} --pathPhastCons=${pathPhyloP_scores} --chr=chr${chr} --pathOutput=${pathResults}/phyloP_${way}_chr${chr}_${suffixExons}.txt" >> ${pathScripts}/log/phyloP_${way}_${species}_${enhancer}_chr${chr}_${suffixExons}
 
       #bash ${pathScripts}/log/phyloP_${way}_${species}_${enhancer}_chr${chr}_${suffixExons}
-      sbatch -p normal --time=1:00:00 --mem=20GB -c 1 -o ${pathScripts}/log/std_output_phyloP_${way}_${species}_${enhancer}_chr${chr}_${suffixExons} -e ${pathScripts}/log/std_error_phyloP_${way}_${species}_${enhancer}_chr${chr}_${suffixExons} ${pathScripts}/log/phyloP_${way}_${species}_${enhancer}_chr${chr}_${suffixExons}
+      sbatch -p normal --time=1:00:00 --mem=40GB -c 1 -o ${pathScripts}/log/std_output_phyloP_${way}_${species}_${enhancer}_chr${chr}_${suffixExons} -e ${pathScripts}/log/std_error_phyloP_${way}_${species}_${enhancer}_chr${chr}_${suffixExons} ${pathScripts}/log/phyloP_${way}_${species}_${enhancer}_chr${chr}_${suffixExons}
        fi
     fi
 done
--- phyloP_scores/formatting.phyloP.output.sh
+++ phyloP_scores/formatting.phyloP.output.sh
@@ -5,7 +5,7 @@
 species=$1
 way=$2
 
-enhancers=(FANTOM5 ENCODE contacted_sequence_original contacted_sequence_simulated)
+enhancers=(FANTOM5 ENCODE restriction_fragments)
 
 if [ "${species}" = "human" ]; then
 enhancers=("${enhancers[@]}" RoadmapEpigenomics FOCS_GRO_seq)
@@ -19,15 +19,15 @@ do
 	path=/beegfs/data/alaverre/Regulatory_landscape/result/phyloP/${species}/${seq}
 	echo "Formatting ${species} ${seq}..."
 
-	if [ -e ${path}/phyloP_${way}_MaskedExons_Ensembl.txt ]; then
+	if [ -e ${path}/phyloP_${way}_MaskedExons_Ensembl94.txt ]; then
 		echo "already done"
 	else
-		grep -hv CoveredLength ${path}/*_${way}_chr* > ${path}/phyloP_${way}_MaskedExons_Ensembl.txt
-		cut -f 2,3,4 ${path}/phyloP_${way}_MaskedExons_Ensembl.txt > ${path}/ID
-		cut -f 2-7 ${path}/phyloP_${way}_MaskedExons_Ensembl.txt > ${path}/other
+		grep -hv CoveredLength ${path}/*_${way}_chr* > ${path}/phyloP_${way}_MaskedExons_Ensembl94.txt
+		cut -f 2,3,4 ${path}/phyloP_${way}_MaskedExons_Ensembl94.txt > ${path}/ID
+		cut -f 2-7 ${path}/phyloP_${way}_MaskedExons_Ensembl94.txt > ${path}/other
 		sed -i "s/\t/:/g" ${path}/ID
-		paste -d "\t" ${path}/ID ${path}/other > ${path}/phyloP_${way}_MaskedExons_Ensembl.txt
-		sed -i '1 i\ID\tChr\tStart\tEnd\tScore\tCoveredLength\tAnalyzedLengthanything' ${path}/phyloP_${way}_MaskedExons_Ensembl.txt
+		paste -d "\t" ${path}/ID ${path}/other > ${path}/phyloP_${way}_MaskedExons_Ensembl94.txt
+		sed -i '1 i\ID\tChr\tStart\tEnd\tScore\tCoveredLength\tAnalyzedLength' ${path}/phyloP_${way}_MaskedExons_Ensembl94.txt
 		rm ${path}/ID ${path}/other ${path}/*_${way}_chr*
 	fi
 done
