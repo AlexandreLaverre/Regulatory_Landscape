69f609f3775987e909e4f36a57a476bf3bf66cd5
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2021-07-29 11:58:59 +0200

    added missing chromosomes

--- estimate_mappability/generate.fake.reads.sh
+++ estimate_mappability/generate.fake.reads.sh
@@ -39,29 +39,32 @@ fi
 
 ##############################################################
 
-for chr in {1..20} X Y 
+for chr in {1..22} X Y 
 do
+    if [ -e ${pathResults}/fake_reads_posinfo_chr${chr}.fa.gz ]; then
+	echo "already done"
+    else
     
-    echo "#!/bin/bash" >  ${pathScripts}/bsub_script_estmap
-
-    if [ ${cluster} = "pbil" ]; then
-	echo "#SBATCH --job-name=estmap_${sp}_${chr}" >>  ${pathScripts}/bsub_script_estmap
-	echo "#SBATCH --partition=normal" >>  ${pathScripts}/bsub_script_estmap
-	echo "#SBATCH --output=${pathScripts}/std_out_${sp}_${chr}_${readlen}" >>  ${pathScripts}/bsub_script_estmap
-	echo "#SBATCH --error=${pathScripts}/std_err_${sp}_${chr}_${readlen}" >>  ${pathScripts}/bsub_script_estmap
-	echo "#SBATCH --cpus-per-task=1" >>  ${pathScripts}/bsub_script_estmap ## 1 CPU
-	echo "#SBATCH --time=5:00:00" >>  ${pathScripts}/bsub_script_estmap ## 5 hours
-	echo "#SBATCH --mem=5G" >>  ${pathScripts}/bsub_script_estmap ## 4g per CPU
+	echo "#!/bin/bash" >  ${pathScripts}/bsub_script_estmap
+	
+	if [ ${cluster} = "pbil" ]; then
+	    echo "#SBATCH --job-name=estmap_${sp}_${chr}" >>  ${pathScripts}/bsub_script_estmap
+	    echo "#SBATCH --partition=normal" >>  ${pathScripts}/bsub_script_estmap
+	    echo "#SBATCH --output=${pathScripts}/std_out_${sp}_${chr}_${readlen}" >>  ${pathScripts}/bsub_script_estmap
+	    echo "#SBATCH --error=${pathScripts}/std_err_${sp}_${chr}_${readlen}" >>  ${pathScripts}/bsub_script_estmap
+	    echo "#SBATCH --cpus-per-task=1" >>  ${pathScripts}/bsub_script_estmap ## 1 CPU
+	    echo "#SBATCH --time=5:00:00" >>  ${pathScripts}/bsub_script_estmap ## 5 hours
+	    echo "#SBATCH --mem=5G" >>  ${pathScripts}/bsub_script_estmap ## 4g per CPU
+	fi
+	
+	echo "perl ${pathScripts}/generate.fake.reads.pl --pathGenome=${pathSequence}/genome_Ensembl${release}.${suffix} --readLength=${readlen} --step=5 --chr=${chr} --pathOutput=${pathResults}/fake_reads_posinfo_chr${chr}.fa" >> ${pathScripts}/bsub_script_estmap
+	
+	echo "gzip ${pathResults}/fake_reads_posinfo_chr${chr}.fa" >> ${pathScripts}/bsub_script_estmap
+	
+	if [ ${cluster} = "pbil" ]; then
+	    sbatch ${pathScripts}/bsub_script_estmap
+	fi
     fi
-    
-    echo "perl ${pathScripts}/generate.fake.reads.pl --pathGenome=${pathSequence}/genome_Ensembl${release}.${suffix} --readLength=${readlen} --step=5 --chr=${chr} --pathOutput=${pathResults}/fake_reads_posinfo_chr${chr}.fa" >> ${pathScripts}/bsub_script_estmap
-
-    echo "gzip ${pathResults}/fake_reads_posinfo_chr${chr}.fa" >> ${pathScripts}/bsub_script_estmap
-    
-    if [ ${cluster} = "pbil" ]; then
-	sbatch ${pathScripts}/bsub_script_estmap
-    fi
-
 done
 
 ##############################################################
