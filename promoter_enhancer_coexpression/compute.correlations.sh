8c4cee7faff8fa2028ff3e257e13d7568c1af3d8
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2021-08-04 13:40:55 +0200

    compute correlations for neighbor enhancers

--- promoter_enhancer_coexpression/compute.correlations.sh
+++ promoter_enhancer_coexpression/compute.correlations.sh
@@ -2,10 +2,14 @@
 
 export sp=$1
 export dataset=$2
+export cluster=$3
 
 ###################################################################################
 
-export path=/beegfs/data/necsulea/RegulatoryLandscapes
+if [ ${cluster} = "pbil" ]; then
+    export path=/beegfs/data/necsulea/RegulatoryLandscapes
+fi
+
 export pathFOCS=${path}/data/FOCS/${sp}/${dataset}/
 export pathResults=${path}/results/co_expression_analysis/${sp}/${dataset}
 export pathScripts=${path}/scripts/promoter_enhancer_coexpression
@@ -24,6 +28,14 @@ if [ -e ${pathResults}/expression_correlations_simulated_data.txt ]; then
     echo "already done"
 else
     perl ${pathScripts}/compute.correlations.pl --pathContacts=${pathResults}/promoters_enhancers_in_contact_simulated_data.txt --pathPromoterExpression=${pathFOCS}/promoter_activity.txt --pathEnhancerExpression=${pathFOCS}/enhancer_activity.txt --pathOutput=${pathResults}/expression_correlations_simulated_data.txt
+fi
+
+###################################################################################
+
+if [ -e ${pathResults}/expression_correlations_neigbor_promoters_enhancers.txt ]; then
+    echo "already done"
+else
+    perl ${pathScripts}/compute.correlations.pl --pathContacts=${pathResults}/neighbor_promoters_enhancers.txt --pathPromoterExpression=${pathFOCS}/promoter_activity.txt --pathEnhancerExpression=${pathFOCS}/enhancer_activity.txt --pathOutput=${pathResults}/expression_correlations_neighbor_promoters_enhancers.txt
 fi
 
 ###################################################################################
