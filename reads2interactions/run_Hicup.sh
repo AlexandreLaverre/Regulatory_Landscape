3eee4cef8bbde0e89b282c818e244f45bfd587ba
Alexandre Laverré <alaverre@pbil-deb.univ-lyon1.fr>
2021-08-09 16:11:16 +0200

    Change paths

--- reads2interactions/run_Hicup.sh
+++ reads2interactions/run_Hicup.sh
@@ -2,19 +2,22 @@
 specie=$1
 genome=$2
 author_ctype=$3
-path_data="/beegfs/data/alaverre/data/reads_PC_HIC/${specie}/${author_ctype}/"
-path_result="/beegfs/data/alaverre/result/HiCup/${specie}/${author_ctype}/"
+
+path=/beegfs/data/alaverre/Regulatory_landscape
+path_data=${path}/data/reads_PC_HIC/${specie}/${author_ctype}/
+path_result=${path}/result/HiCup/${specie}/${author_ctype}/
 
 mkdir -p ${path_result}
+
 ## Config files for HiCUP ##
 echo "Outdir: ${path_result}" > ${path_result}/config_files.txt
 echo "Zip: 1" >> ${path_result}/config_files.txt
 echo "Keep: 0" >> ${path_result}/config_files.txt
-echo "Threads: 24" >> ${path_result}/config_files.txt
-echo "Bowtie2: /beegfs/home/alaverre/Tools/bowtie2/bowtie2" >> ${path_result}/config_files.txt 
-echo "Digest: /beegfs/data/alaverre/data/genome_ref/Digest_${genome}_HindIII_None.txt" >> ${path_result}/config_files.txt
-echo "Index: /beegfs/data/alaverre/data/genome_ref/${genome}" >> ${path_result}/config_files.txt
-echo "R: /beegfs/data/soft/R-3.5.1/bin/R" >> ${path_result}/config_files.txt
+echo "Threads: 30" >> ${path_result}/config_files.txt
+echo "Bowtie2: /beegfs/data/alaverre/Tools/bowtie2/bowtie2" >> ${path_result}/config_files.txt 
+echo "Digest: ${path}/data/genome_ref/Digest_${genome}_HindIII_None.txt" >> ${path_result}/config_files.txt
+echo "Index: ${path}/data/genome_ref/${genome}" >> ${path_result}/config_files.txt
+echo "R: /beegfs/data/soft/R-3.5.2/bin/R" >> ${path_result}/config_files.txt
 echo "Shortest: 150" >> ${path_result}/config_files.txt
 echo "Longest: 800" >> ${path_result}/config_files.txt
 
@@ -29,8 +32,15 @@ sed -i '$d' ${path_result}/config_files.txt
 
 ## HiCUP ##
 echo "#!/bin/bash" > ${path_result}/bsub_hicup 
+echo "#SBATCH --job-name=HiCup_${author_ctype}" >>  ${path_result}/bsub_hicup 
+echo "#SBATCH --partition=normal" >>  ${path_result}/bsub_hicup 
 echo "#SBATCH -o ${path_result}/std_output.txt" >> ${path_result}/bsub_hicup
-echo "#SBATCH -e ${path_result}/std_error.txt" >> ${path_result}/bsub_hicup	
+echo "#SBATCH -e ${path_result}/std_error.txt" >> ${path_result}/bsub_hicup
+echo "#SBATCH --cpus-per-task=30" >>  ${path_result}/bsub_hicup
+echo "#SBATCH --time=30:00:00" >>  ${path_result}/bsub_hicup
+echo "#SBATCH --mem=20G" >>  ${path_result}/bsub_hicup
+
 echo "source /beegfs/home/alaverre/.bashrc" >> ${path_result}/bsub_hicup
 echo "hicup --config  ${path_result}/config_files.txt" >> ${path_result}/bsub_hicup
-sbatch -p normal --time=30:00:00 --mem=10GB -c 24 ${path_result}/bsub_hicup
+
+sbatch  ${path_result}/bsub_hicup
