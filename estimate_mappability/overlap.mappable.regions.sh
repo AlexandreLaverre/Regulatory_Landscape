a5ad28eccebc2c6130cdf44f8a7364fc53f387f4
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2021-08-09 09:40:38 +0200

    larger margin for mappable regions

--- estimate_mappability/overlap.mappable.regions.sh
+++ estimate_mappability/overlap.mappable.regions.sh
@@ -27,6 +27,8 @@ fi
 
 #################################################################################
 
-perl ${pathScripts}/overlap.mappable.regions.pl --pathRestrictionFragments=${pathFragments}/frag_coords_${genome}.bed --pathMappedRegions=${pathResults}/mapped_regions.txt --margin=500 --step=10 --pathOutput=${pathResults}/overlap_restriction_fragments_mapped_regions_margin500_step10.txt
+## perl ${pathScripts}/overlap.mappable.regions.pl --pathRestrictionFragments=${pathFragments}/frag_coords_${genome}.bed --pathMappedRegions=${pathResults}/mapped_regions.txt --margin=500 --step=10 --pathOutput=${pathResults}/overlap_restriction_fragments_mapped_regions_margin500_step10.txt
+
+perl ${pathScripts}/overlap.mappable.regions.pl --pathRestrictionFragments=${pathFragments}/frag_coords_${genome}.bed --pathMappedRegions=${pathResults}/mapped_regions.txt --margin=1000 --step=10 --pathOutput=${pathResults}/overlap_restriction_fragments_mapped_regions_margin1000_step10.txt
 
 #################################################################################
