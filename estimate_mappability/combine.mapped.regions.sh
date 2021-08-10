3684bdc938ff4e88ee11bed880fc93715c65a8fb
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2021-08-10 10:52:00 +0200

    combine mapped regions

---
+++ estimate_mappability/combine.mapped.regions.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+export sp=$1
+export readlen=$2
+export cluster=$3
+
+##############################################################
+
+if [ ${cluster} = "pbil" ]; then
+    export path=/beegfs/data/${USER}/RegulatoryLandscapes
+fi
+
+export pathGenomeIndexes=${path}/data/genome_sequences/${sp}
+export pathResults=${path}/results/mappability/readlength${readlen}/${sp}
+export pathScripts=${path}/scripts/estimate_mappability
+
+##############################################################
+
+if [ -e ${pathResults}/mapped_regions.txt ]; then
+    echo "result file already there, not doing anything"
+fi
+
+##############################################################
+
+for chr in {1..32} X Y Z W 
+do
+    if [ -e ${pathResults}/fake_reads_chr${chr}_mappedregions.txt ]; then
+	cat ${pathResults}/fake_reads_chr${chr}_mappedregions.txt >> ${pathResults}/mapped_regions.txt
+    fi
+done
+
+##############################################################
