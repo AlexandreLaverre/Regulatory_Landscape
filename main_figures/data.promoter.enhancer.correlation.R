cfcb90908a7480ce4283ffcea0fd60fb78344e6b
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2021-08-11 19:53:13 +0200

    removed neighbor enhancers from promoter-enhancer corr

--- main_figures/data.promoter.enhancer.correlation.R
+++ main_figures/data.promoter.enhancer.correlation.R
@@ -28,8 +28,7 @@ for(ref_sp in c("human", "mouse")){
   
   obs_correl_activity_dist <- list()
   simul_correl_activity_dist <- list()
-  neighbors_correl_activity_dist <- list()
-  
+    
   for (enh in enhancers){
     if(file.exists(paste(pathFinalData, "SupplementaryDataset8", ref_sp, enh, "expression_correlations_real_data.txt", sep="/")) & file.exists(paste(pathFinalData, "SupplementaryDataset8", ref_sp, enh, "expression_correlations_simulated_data.txt", sep="/"))){
 
@@ -54,30 +53,16 @@ for(ref_sp in c("human", "mouse")){
       simul$IDContact=paste(simul$IDBait, simul$IDContactedFragment, sep="-")
 
       simul=simul[which(simul$IDContact%in%frag.contact.sim$idcontact),]
-
-      ## neighbor enhancers
-
-       neighbors <- fread(paste(pathFinalData, "SupplementaryDataset8", ref_sp, enh, "expression_correlations_neighbor_promoters_enhancers.txt", sep="/"), h=T, sep="\t")
-      class(neighbors)<-"data.frame"
-
-      ## filter for distances
-
-      neighbors=neighbors[which(neighbors$Distance>=minDistance & neighbors$Distance<=maxDistance),]
-
-      ## select the same promoters as for the observed contacts
-
-      neighbors=neighbors[which(neighbors$IDPromoter%in%obs$IDPromoter),]
-
+     
       print(paste(ref_sp, enh, "observed mean corr, all distances", mean(obs$SpearmanCorrelation)))
       print(paste(ref_sp, enh, "simulated mean corr, all distances", mean(simul$SpearmanCorrelation)))
-      print(paste(ref_sp, enh, "neighbors mean corr, all distances", mean(neighbors$SpearmanCorrelation)))
       print(wilcox.test(obs$SpearmanCorrelation, simul$SpearmanCorrelation))
             
       # according to distance
     
       obs$dist_class <-cut(obs$Distance, breaks=seq(from=minDistance, to=maxDistance+50000, by=50000), include.lowest = T)
       simul$dist_class <- cut(simul$Distance, breaks=seq(from=minDistance, to=maxDistance+50000, by=50000), include.lowest = T)
-      neighbors$dist_class <- cut(neighbors$Distance, breaks=seq(from=minDistance, to=maxDistance+50000, by=50000), include.lowest = T)
+    
 
       BC.obs=tapply(obs$SpearmanCorrelation, obs$dist_class, function(x) BCa(x, delta=NA, M=100, theta=mean, na.rm=T))
       obs_correl_activity_dist[[enh]]=unlist(lapply(BC.obs, function(x) x[3]))
@@ -89,14 +74,11 @@ for(ref_sp in c("human", "mouse")){
       simul_correl_activity_dist[[paste0(enh, "_conflow")]]=unlist(lapply(BC.simul, function(x) x[4]))
       simul_correl_activity_dist[[paste0(enh, "_confup")]]=unlist(lapply(BC.simul, function(x) x[5]))
 
-      BC.neighbors=tapply(neighbors$SpearmanCorrelation, neighbors$dist_class, function(x) BCa(x, delta=NA, M=100, theta=mean, na.rm=T))
-      neighbors_correl_activity_dist[[enh]]=unlist(lapply(BC.neighbors, function(x) x[3]))
-      neighbors_correl_activity_dist[[paste0(enh, "_conflow")]]=unlist(lapply(BC.neighbors, function(x) x[4]))
-      neighbors_correl_activity_dist[[paste0(enh, "_confup")]]=unlist(lapply(BC.neighbors, function(x) x[5]))
+     
     }
   }
   
-  correl_activity <- list(obs=obs_correl_activity_dist, simul=simul_correl_activity_dist, neighbors=neighbors_correl_activity_dist)
+  correl_activity <- list(obs=obs_correl_activity_dist, simul=simul_correl_activity_dist)
   
 ################################################# Save RData ################################################# 
   
