45390e3bb5bbebbb5f3564eb68037f1a32a4e2ce
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2021-08-11 19:53:25 +0200

    added regulatory regions

--- main_figures/data.neighbor.enhancers.R
+++ main_figures/data.neighbor.enhancers.R
@@ -2,7 +2,7 @@
 
 source("parameters.R")
 
-pathNeighborEnhancers="../../results/neighbor_enhancers/"
+pathNeighborEnhancers=paste(pathFinalData, "SupplementaryDataset4/",sep="")
 
 load(paste(pathFigures, "RData/data.gene.annotations.RData", sep=""))
 
@@ -21,33 +21,21 @@ for(sp in c("human", "mouse")){
   ## gene enhancer contacts
   
   gene.enhancer.contacts[[sp]]=list()
+
+  regions=read.table(paste(pathNeighborEnhancers, sp, "/predicted_regulatory_regions_neighbor_TSS.txt", sep=""), h=T, stringsAsFactors=F, sep="\t")
+
+  gene.enhancer.contacts[["regions"]]=regions
   
   for(enh in enhancer.datasets[[sp]]){
     
     print(enh)
     
-    real=read.table(paste(pathNeighborEnhancers, sp, "/", enh, "/neighbor_enhancers_Ensembl94.txt", sep=""), h=T, stringsAsFactors=F)
+    real=read.table(paste(pathNeighborEnhancers, sp, "/", enh, "/predicted_enhancers_neighbor_TSS.txt", sep=""), h=T, stringsAsFactors=F)
     
     ## select protein-coding genes
 
-    real=real[which(real$GeneID%in%pc.genes),]
-
-    ## select enhancers in our accepted distance range
+    real=real[which(real$GeneID%in%pc.genes),] 
        
-    real=real[which(real$Distance>=minDistance & real$Distance<=maxDistance),]
-
-    ## change column names
-
-    colnames(real)[which(colnames(real)=="EnhancerID")]="enhancer"
-    colnames(real)[which(colnames(real)=="GeneID")]="gene"
-
-    ## add chr to enhancer id
-
-    g=grep("^chr", real$enhancer)
-
-    if(length(g)==0){
-      real$enhancer=paste("chr", real$enhancer, sep="")
-    }
     ## save data
     
     gene.enhancer.contacts[[sp]][[enh]]=list("real"=real)
