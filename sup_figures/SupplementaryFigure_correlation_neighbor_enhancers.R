6a2565910f1ca14454dfb4502acd5288279145a5
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2021-08-13 08:57:57 +0200

    supp fig corr neighbor enhancers, final version

--- sup_figures/SupplementaryFigure_correlation_neighbor_enhancers.R
+++ sup_figures/SupplementaryFigure_correlation_neighbor_enhancers.R
@@ -44,8 +44,8 @@ for(sp in c("human", "mouse")){
     
     if(length(obs[[sp]][[enh]])>0){
       
-      ymin=min(c(obs[[sp]][[paste0(enh,"_conflow")]], obs[[sp]][[paste0(enh,"_confup")]], neighbors[[sp]][[paste0(enh,"_conflow")]], neighbors[[sp]][[paste0(enh,"_confup")]]))
-      ymax=max(c(obs[[sp]][[paste0(enh,"_conflow")]], obs[[sp]][[paste0(enh,"_confup")]], neighbors[[sp]][[paste0(enh,"_conflow")]], neighbors[[sp]][[paste0(enh,"_confup")]]))
+      ymin=min(c(obs[[sp]][[paste0(enh,"_conflow")]], obs[[sp]][[paste0(enh,"_confup")]], neighbors[[sp]][[paste0(enh,"_conflow")]], neighbors[[sp]][[paste0(enh,"_confup")]]), na.rm=T)
+      ymax=max(c(obs[[sp]][[paste0(enh,"_conflow")]], obs[[sp]][[paste0(enh,"_confup")]], neighbors[[sp]][[paste0(enh,"_conflow")]], neighbors[[sp]][[paste0(enh,"_confup")]]), na.rm=T)
       
       ylim=c(ymin, ymax)
       xlim=c(0.5, length(obs[[sp]][[enh]])+0.5)
@@ -60,15 +60,19 @@ for(sp in c("human", "mouse")){
       segments(xpos, obs[[sp]][[paste0(enh,"_conflow")]], xpos, obs[[sp]][[paste0(enh,"_confup")]], col=dataset.colors["Original"])
       segments(xpos, neighbors[[sp]][[paste0(enh,"_conflow")]], xpos, neighbors[[sp]][[paste0(enh,"_confup")]], col="black")
             
-      class_leg <- c("0", "0.25", "0.5", "0.75", "1")
-      axis(side=1, at=c(0.5, 8.5, 19.5, 29.5, 39.5), labels=class_leg, mgp=c(3, 0.65, 0), cex.axis=0.95)
+      class_leg <- c("0.05", "0.25", "0.5", "0.75", "1")
+      axis(side=1, at=c(0.5, 9.5, 19.5, 29.5, 39.5), labels=class_leg, mgp=c(3, 0.65, 0), cex.axis=0.95)
       axis(side=2, mgp=c(3, 0.65, 0), cex.axis=0.95, las=2)
       
-      mtext("Spearman's rho", side=2, cex=0.8, line=2.5, at=(ymin+ymax)*0.9/2)
-      mtext("distance to promoters (Mb)", side=1, line=2, cex=0.8)
+      mtext("Spearman's rho", side=2, cex=0.75, line=2.5, at=(ymin+ymax)*0.9/2)
+      mtext("distance to promoters (Mb)", side=1, line=2, cex=0.75)
       mtext(paste(sp, enh), side=3, line=0.5, cex=0.8)
 
-      mtext(labels[index], side=3, line=0.5, font=2, at=-10)
+      mtext(labels[index], side=3, line=0.5, font=2, at=-11.5)
+
+      if(index==1){
+        legend("bottomleft", bty="n", col=c("darkorange", "black"), legend=c("contacts", "neighbors"), inset=0.01, pch=20)
+      }
       
       index=index+1
     }
