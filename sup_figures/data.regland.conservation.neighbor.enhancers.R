cbbb48967ccb7694d0cbcf52cdb2b9541d83fe34
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2021-08-16 03:27:35 +0200

    added synteny in regland cons for neighbor enhancers

--- sup_figures/data.regland.conservation.neighbor.enhancers.R
+++ sup_figures/data.regland.conservation.neighbor.enhancers.R
@@ -19,9 +19,11 @@ print("done")
 
 #######################################################################################
 
-## minDistance and maxDistance defined in parameters.R
+dist.classes=c("all",  "neighbors")
+min.distances=c(minDistance, minDistance)
+max.distances=c(maxDistance, 100000)
 
-## min and max number of enhancers for contacts and synteny conservation
+## min and max number of enhancers for synteny conservation
 
 min.nb=5
 max.nb=100
@@ -48,49 +50,78 @@ for(ref in c("human", "mouse")){
   
   all.genes=intersect(all.genes, baited.genes)
 
-  for(enh in enhancer.datasets[[ref]]){
+  enh="ENCODE"
   
-    print(paste(ref, enh))
+  print(paste(ref, enh))
+  
+  ## we start from the gene-enhancer data
+  
+  contacts=gene.enhancer.contacts[[ref]][[enh]][["real"]]
+  contacts$id=paste(contacts$GeneID, contacts$EnhancerID, sep="-")
     
-    ## we start from the gene-enhancer contact data
-    
-    contacts=gene.enhancer.contacts[[ref]][[enh]][["real"]]
-      
-    ## we select only previously filtered orthologous genes
-    
-    contacts=contacts[which(contacts$gene%in%ortho[,ref]),]
+  ## we select only previously filtered orthologous genes
+  
+  contacts=contacts[which(contacts$GeneID%in%ortho[,ref]),]
+  
+  ## same minimum distance as for the PCHi-C contacts
+  
+  contacts=contacts[which(contacts$Distance>=minDistance),]
+  
+  ## sequence conservation column
+  
+  load(paste(pathFigures, "RData/data.sequence.conservation.enhancers.",enh,".",ref,"2", tg,".RData", sep=""))
+  contacts$align_score=pcungapped[contacts$enhancer]
 
-    ## we add sequence conservation column
-
-    load(paste(pathFigures, "RData/data.sequence.conservation.enhancers.",enh,".",ref,"2", tg,".RData", sep=""))
-    contacts$align_score=pcungapped[contacts$enhancer]
-   
+  ## synteny conservation
+  load(paste(pathFigures, "RData/data.synteny.conservation.neighbor.enhancers.RData",sep=""))
+  synteny=synteny.cons[[paste(ref, "2", tg, sep="")]]
+  synteny$id=paste(synteny$GeneID, synteny$EnhancerID, sep="-")
+  
+  ## prepare result list
+  
+  results=list("gene"=all.genes)
+  
+  for(i in 1:length(dist.classes)){
     
-    ## prepare result list
-
-    results=list("gene"=all.genes)
+    ## select interactions in a given distance range
     
-    ## nb enhancers by gene
+    dist.class=dist.classes[i]
+    min.dist=min.distances[i]
+    max.dist=max.distances[i]
     
-    nb.contacts=as.numeric(table(factor(contacts$gene, levels=all.genes)))
+    print(dist.class)
     
-    ## divide nb of contacts into 5 classes
-    
-    results[["nb.contacts.all"]]=nb.contacts
-    results[["class.nb.contacts.all"]]=cut(nb.contacts, breaks=c(1, 10, 20, 30, 40, max(nb.contacts)), include.lowest=T, labels=c("1-10", "11-20", "21-30", "30-40", ">40"))
+    filtered.contacts=contacts[which(contacts$dist>=min.dist & contacts$dist<=max.dist),]
     
     ## mean conservation score by gene
     
-    mean.aln.score=tapply(contacts$align_score, factor(contacts$gene, levels=all.genes), mean, na.rm=T)
+    mean.aln.score=tapply(filtered.contacts$align_score, factor(filtered.contacts$GeneID, levels=all.genes), mean, na.rm=T)
     
-    results[["mean.aln.score.all"]]=mean.aln.score
-    results[["class.aln.score.all"]]=cut(mean.aln.score, breaks=seq(from=0, to=1, length=6), include.lowest=T)
+    results[[paste("mean.aln.score", dist.class, sep=".")]]=mean.aln.score
+    results[[paste("class.aln.score", dist.class, sep=".")]]=cut(mean.aln.score, breaks=seq(from=0, to=1, length=6), include.lowest=T)
     
+    ## synteny conservation
     
-    results=as.data.frame(results)
+    filtered.synteny=synteny[which(synteny$id%in%filtered.contacts$id),]
     
-    regland.conservation[[ref]][[enh]]=results
+    fr.cons.synt=tapply(filtered.synteny$ConservedSynteny,  factor(filtered.synteny$GeneID, levels=all.genes), function(x) length(which(x=="yes"))/length(x))
+    
+    nb.enh.synt=as.numeric(table(factor(filtered.synteny$GeneID, levels=all.genes)))
+    
+    ## if fewer than min.nb enhancers, we assign NA values to synteny conservation
+    
+    fr.cons.synt[which(nb.enh.synt < min.nb | nb.enh.synt > max.nb)]=NA
+    
+    results[[paste("fr.synteny.cons",dist.class,sep=".")]]=fr.cons.synt
+    
+    results[[paste("class.synteny.cons", dist.class, sep=".")]]=factor(fr.cons.synt<1,levels=c(TRUE, FALSE), labels=c("broken", "conserved"))
+    
   }
+  
+  results=as.data.frame(results)
+  
+  regland.conservation[[ref]]=results
+  
 }
 
 #######################################################################################
