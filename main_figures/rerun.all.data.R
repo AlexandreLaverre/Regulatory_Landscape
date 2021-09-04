3d5cb155abdcc52097f49fd450d496d077abedb3
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2021-09-04 23:49:20 +0300

    added mising script

--- main_figures/rerun.all.data.R
+++ main_figures/rerun.all.data.R
@@ -19,19 +19,22 @@ data.scripts=c(data.scripts, "data.aberrant.fragments.R") ## depends on sample i
 data.scripts=c(data.scripts, "data.enhancer.statistics.R") ## depends on fragment statistics
 data.scripts=c(data.scripts, "data.fragment.contacts.R") ## depends on fragment statistics
 data.scripts=c(data.scripts, "data.sequence.conservation.fragments.R") ## depends on fragment statistics
-data.scripts=c(data.scripts, "data.promoter.enhancer.correlation.R")  ## depends on fragment contacts
+data.scripts=c(data.scripts, "data.promoter.enhancer.correlation.R")  ## depends on fragment contacts ## BOOTBCA
 data.scripts=c(data.scripts, "data.CM2019.SomaticOrgans.expression.divergence.R") ## depends on gene annotations
+data.scripts=c(data.scripts, "data.CM2019.AllOrgans.expression.divergence.R") ## depends on gene annotations
 
 ## 2 dependencies
 data.scripts=c(data.scripts, "data.sample.clustering.R") ## sample info, fragment contacts
-data.scripts=c(data.scripts, "data.enhancer.coverage.R") ## fragment statistics, fragment contacts
+data.scripts=c(data.scripts, "data.enhancer.coverage.R") ## fragment statistics, fragment contacts ## BOOTBCA
 data.scripts=c(data.scripts, "data.phyloP.scores.R") ## fragment statistics, enhancer statistics
 data.scripts=c(data.scripts, "data.AFC.R") ## fragment contacts, sample info
-data.scripts=c(data.scripts, "data.bootstrap.nb.cell.types.R") ## fragment contacts, sample info
+
+data.scripts=c(data.scripts, "data.bootstrap.nb.cell.types.R") ## fragment contacts, sample info ## BOOTBCA
 
 ## 3 dependencies
-data.scripts=c(data.scripts, "data.bootstrap.conservation.distance.repeats.R") ## sequence conservation, enhancer statistics, fragment statistics
-
+data.scripts=c(data.scripts, "data.bootstrap.conservation.distance.repeats.R") ## sequence conservation, enhancer statistics, fragment statistics  ## BOOTBCA
+data.scripts=c(data.scripts, "data.bootstrap.phyloP.distance.R") ## enhancer statistics, fragment statistics, sequence conservation ## BOOTBCA
+ 
 ## 4 dependencies
 data.scripts=c(data.scripts, "data.gene.enhancer.contacts.R") ## gene annotations, sample info,  fragment contacts, enhancer statistics
 data.scripts=c(data.scripts, "data.contact.conservation.enhancers.R") ## gene-enhancer contacts, ortho genes, bait annotations,  sequence conservation for enhancers
